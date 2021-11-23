ff16844f3c45763737fa8688602a64313df897c6
Kinplemelon <wujiu529@gmail.com>
2021-11-23 11:31:40 +0800

    refactor(overview): use setup script and ts in nodes graph and simple line components

--- src/views/Dashboard/components/SimpleLine.vue
+++ src/views/Dashboard/components/SimpleLine.vue
@@ -1,8 +1,18 @@
 <template>
-  <div ref="chart" class="simple-line"></div>
+  <div ref="chartEl" class="simple-line"></div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SimpleLine",
+});
+</script>
+
+<script lang="ts" setup>
+import { defineProps, ref, reactive, watch, onMounted, Ref } from "vue";
+import { useStore } from "vuex";
 import * as echarts from "echarts/lib/echarts";
 import "echarts/lib/chart/line";
 import "echarts/lib/chart/bar";
@@ -12,121 +22,113 @@ import "echarts/lib/component/title";
 import "echarts/lib/component/markLine";
 import "echarts/lib/component/markPoint";
 
-import resizeChart from "@/mixins/resizeChart";
+const store = useStore();
 
-export default {
-  name: "SimpleLine",
+const props = defineProps({
+  value: {
+    type: Object,
+    default: () => ({ x: [], y: [] }),
+    required: false,
+  },
+  color: {
+    type: String,
+    default: "#975fe4",
+  },
+  type: {
+    type: String,
+    default: "line",
+  },
+});
 
-  mixins: [resizeChart],
-
-  props: {
-    value: {
-      type: Object,
-      default: () => ({ x: [], y: [] }),
-      required: false,
-    },
-    color: {
-      type: String,
-      default: "#975fe4",
-    },
-    type: {
-      type: String,
-      default: "line",
+const chartEl = ref();
+const chart: Ref<any> = ref(null);
+const option: any = reactive({
+  color: [props.color],
+  grid: {
+    x: 0, // 默认是80px
+    y: 0, // 默认是60px
+    x2: 0, // 默认80px
+    y2: 0, // 默认60px
+  },
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      type: "none",
     },
   },
+  xAxis: {
+    type: "category",
+    // boundaryGap: false,
+    data: props.value.x,
+    triggerEvent: true,
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      show: false,
+    },
+  },
+  yAxis: {
+    type: "value",
+    splitLine: {
+      show: false,
+    },
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      show: false,
+    },
+  },
+  series: [],
+});
 
-  data() {
-    return {
-      chart: null,
-      option: {
-        color: [this.color],
-        grid: {
-          x: 0, // 默认是80px
-          y: 0, // 默认是60px
-          x2: 0, // 默认80px
-          y2: 0, // 默认60px
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "none",
-          },
-        },
-        xAxis: {
-          type: "category",
-          // boundaryGap: false,
-          data: this.value.x,
-          triggerEvent: true,
-          axisLine: {
-            show: false,
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            show: false,
-          },
-        },
-        yAxis: {
-          type: "value",
-          splitLine: {
-            show: false,
-          },
-          axisLine: {
-            show: false,
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            show: false,
-          },
-        },
-        series: [],
+watch(
+  () => props.value,
+  () => {
+    setSeriesConfig();
+  }
+);
+
+watch(
+  () => store.state.leftBarCollapse,
+  () => {
+    setTimeout(setSeriesConfig, 500);
+  }
+);
+
+const setSeriesConfig = () => {
+  const { color, type } = props;
+  let Dom = chartEl.value;
+  if (!Dom) return;
+  echarts.dispose(Dom);
+  chart.value = echarts.init(Dom);
+  option.series = [
+    {
+      data: props.value.y,
+      smooth: true,
+      type,
+      symbolSize: 0,
+      lineStyle: {
+        color,
       },
-    };
-  },
-
-  watch: {
-    value: {
-      deep: true,
-      handler: "setSeriesConfig",
+      label: {
+        show: false,
+      },
+      areaStyle: {
+        color,
+        opacity: 1,
+      },
     },
-    "$store.state.leftBarCollapse": function () {
-      setTimeout(this.setSeriesConfig, 500);
-    },
-  },
-  mounted() {
-    this.setSeriesConfig();
-  },
-  methods: {
-    setSeriesConfig() {
-      const { color } = this;
-      const { type } = this;
-      let Dom = this.$refs.chart;
-      if (!Dom) return;
-      echarts.dispose(Dom);
-      this.chart = echarts.init(Dom);
-      this.option.series = [
-        {
-          data: this.value.y,
-          smooth: true,
-          type,
-          symbolSize: 0,
-          lineStyle: {
-            color,
-          },
-          label: {
-            show: false,
-          },
-          areaStyle: {
-            color,
-            opacity: 1,
-          },
-        },
-      ];
-      this.chart.setOption(this.option);
-    },
-  },
+  ];
+  chart.value?.setOption(option);
 };
+
+onMounted(setSeriesConfig);
 </script>
