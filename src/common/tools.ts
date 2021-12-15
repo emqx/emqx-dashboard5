b02dd9791efad60e456a397c102fc05e1e947d2a
Kinplemelon <wujiu529@gmail.com>
2021-12-15 14:56:57 +0800

    fix(tools): Fixed the error when run CI

--- src/common/tools.ts
+++ src/common/tools.ts
@@ -1,6 +1,6 @@
+import { useI18n } from "vue-i18n";
 import Clipboard from "clipboard";
 import { ElMessage } from "element-plus";
-import i18n from "@/i18n";
 
 export const checkStringWithUnit = (str: string, units: Array<string>): boolean => {
   const reg = new RegExp(`^\\d+(.\\d+)?(${units.join("|")})$`);
@@ -10,10 +10,9 @@ export const checkStringWithUnit = (str: string, units: Array<string>): boolean 
 export const checkInRange = (val: number, min: number, max: number): boolean => val >= min && val <= max;
 
 export const createClipboardEleWithTargetText = (btn: HTMLElement, text: string) => {
+  const { t } = useI18n();
   const clipboard = new Clipboard(btn, { text: () => text });
-  clipboard.on("success", () =>
-    ElMessage.success(i18n.global.t("Base.copied"))
-  );
-  clipboard.on("error", () => ElMessage.error(i18n.global.t("Base.opErr")));
+  clipboard.on("success", () => ElMessage.success(t("Base.copied")));
+  clipboard.on("error", () => ElMessage.error(t("Base.opErr")));
   return clipboard;
 };
