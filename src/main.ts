1c8f41a36707722b1db65810848d8fa6160e09a8
Kinplemelon <wujiu529@gmail.com>
2021-12-22 11:34:28 +0800

    feat(main): add element i18n

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from "./router";
 import store from "./store";
 import "@/style/common.scss";
 import ElementPlus from "element-plus";
+import elementI18nZhCn from "element-plus/es/locale/lang/zh-cn";
+import elementI18nEn from "element-plus/es/locale/lang/en";
+
 import i18n from "./i18n";
 // import "@/style/element-reset.scss";
 // import directive from "@/common/directive";
@@ -13,10 +16,14 @@ function globalComponents(app: Application) {
   app.component(EMQSelect.name, EMQSelect);
 }
 
+const elementLang = store.state.lang === "en" ? elementI18nEn : elementI18nZhCn;
+
 createApp(App)
   .use(store)
   .use(router)
-  .use(ElementPlus)
+  .use(ElementPlus, {
+    locale: elementLang,
+  })
   .use(i18n)
   .use(globalComponents)
   .mount("#app");
