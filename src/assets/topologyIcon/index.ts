70a55ceedf8031e02e6785cc0e104887c9d2a389
Kinplemelon <wujiu529@gmail.com>
2022-02-16 15:41:42 +0800

    feat(rule): add img to node and add some mode config

---
+++ src/assets/topologyIcon/index.ts
@@ -0,0 +1,17 @@
+import bridgeHTTP from './bridge-http.svg'
+import bridgeMQTT from './bridge-mqtt.svg'
+import console from './console.svg'
+import event from './event.svg'
+import republish from './republish.svg'
+import rule from './rule.svg'
+import topic from './topic.svg'
+
+export default {
+  'bridge-http': bridgeHTTP,
+  'bridge-mqtt': bridgeMQTT,
+  console,
+  event,
+  republish,
+  rule,
+  topic,
+}
