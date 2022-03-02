975769ffbf613bc3f86f440eb05fec85c3115d81
YuShifan <894402575bt@gmail.com>
2022-03-02 15:55:26 +0800

    feat(components): add array input component

---
+++ src/components/InputArray.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-input
+    class="input-array"
+    v-model.trim="inputValue"
+    type="textarea"
+    @blur="handleBlur"
+  ></el-input>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InputArray',
+  props: {
+    value: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      inputValue: '',
+    }
+  },
+  methods: {
+    handleBlur() {
+      this.$emit('input', this.inputValue.split(','))
+    },
+  },
+  created() {
+    this.inpputValue = this.value.join(',')
+  },
+})
+</script>
