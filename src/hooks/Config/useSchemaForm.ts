74a41f178e82b136339ad5f0f2184e667a7809ea
YuShifan <894402575bt@gmail.com>
2022-03-04 15:58:03 +0800

    feat(schemaForm): add flatten and unflatten configs functions

--- src/hooks/Config/useSchemaForm.ts
+++ src/hooks/Config/useSchemaForm.ts
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { inject, ref, Ref, watch } from 'vue'
 import _ from 'lodash'
 import { InjectSchema, Properties, Component, Schema } from '@/types/schemaForm'
 
 export default function useSchemaForm(path: string): {
   schema: InjectSchema
-  components: Ref
+  components: Ref<Properties>
+  flattenConfigs: (
+    obj: any,
+    prefix?: any[],
+    current?: {
+      [key: string]: any
+    },
+  ) => {
+    [key: string]: any
+  }
+  unflattenConfigs: (obj: { [key: string]: any }) => any
 } {
   const schema: InjectSchema = inject('schema')
   const components = ref<Properties>({})
@@ -42,7 +53,41 @@ export default function useSchemaForm(path: string): {
   const handleInjectChanged = (data: Schema) => {
     components.value = getComponents(data)
   }
+  // { a: { b: c: 1 } } => { 'a.b.c': 1 }
+  const flattenConfigs = (
+    obj: { [key: string]: any },
+    prefix: any[] = [],
+    current: { [key: string]: any } = {},
+  ) => {
+    if (typeof obj === 'object' && !Array.isArray(obj) && obj !== null) {
+      for (const key of Object.keys(obj)) {
+        flattenConfigs(obj[key], prefix.concat(key), current)
+      }
+    } else {
+      current[prefix.join('.')] = obj
+    }
+    return current
+  }
+  // { 'a.b.c': 1 } => { a: { b: { c: 1 } } }
+  const unflattenConfigs = (obj: { [key: string]: any }) => {
+    if (Object(obj) !== obj && !Array.isArray(obj)) return obj
+    const regex = /\.?([^.[\]]+)|\[(\d+)\]/g
+    const resultholder: { [key: string]: any } = {}
+    for (const p in obj) {
+      let current = resultholder
+      let prop = ''
+      let m: any
+      while ((m = regex.exec(p))) {
+        current = current[prop] || (current[prop] = m[2] ? [] : {})
+        prop = m[2] || m[1]
+      }
+      current[prop] = obj[p]
+    }
+    return resultholder[''] || resultholder
+  }
   return {
+    flattenConfigs,
+    unflattenConfigs,
     schema,
     components,
   }
