aa6cc8276ee454012d97e385738e62281e4f5159
YuShifan <894402575bt@gmail.com>
2022-03-04 16:49:54 +0800

    fix(schemaForm): fix can not bind config nested data

--- src/types/schemaForm.d.ts
+++ src/types/schemaForm.d.ts
@@ -10,7 +10,7 @@ export interface Properties {
     type: string
     maximum?: number
     minimum?: number
-    parent?: string
+    path?: string
     readOnly?: boolean
     items: {
       type: string
