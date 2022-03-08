33e28c0be34f00356fa83d11b8f4416dbdcf8905
YuShifan <894402575bt@gmail.com>
2022-03-08 10:35:47 +0800

    feat(log): add log config page

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,11 @@
 import http from '@/common/http'
-import { Cluster } from '@/types/config'
+import { Cluster, Log } from '@/types/config'
 
 export const getClusterConfigs = (): Promise<Cluster> => http.get('/configs/cluster')
 
 export const updateClusterConfigs = (data: Cluster): Promise<Cluster> =>
   http.put('/configs/cluster', data)
+
+export const getLogConfigs = (): Promise<Log> => http.get('/configs/log')
+
+export const updateLogConfigs = (data: Log): Promise<Log> => http.put('/configs/log', data)
