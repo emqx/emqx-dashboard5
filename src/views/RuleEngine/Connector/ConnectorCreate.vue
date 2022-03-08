77266b5fd15174715ca6a15a8ed2c1a1e3798a35
Kinplemelon <wujiu529@gmail.com>
2022-03-08 11:11:54 +0800

    feat(rule): handle jump events when add output

--- src/views/RuleEngine/Connector/ConnectorCreate.vue
+++ src/views/RuleEngine/Connector/ConnectorCreate.vue
@@ -1,79 +1,80 @@
 <template>
-  <div class="data-bridge-create">
-    <div class="page-header-title">
-      {{ tl('createConnector') }}
+  <div class="app-wrapper">
+    <div class="data-bridge-create">
+      <div class="page-header-title">
+        {{ tl('createConnector') }}
+      </div>
+      <el-row>
+        <el-col :span="12">
+          <el-steps :active="stepActive" finish-status="success">
+            <el-step :title="tl('connectorType')"> </el-step>
+            <el-step :title="tl('configuration')"></el-step>
+          </el-steps>
+        </el-col>
+      </el-row>
+      <el-row class="config-body">
+        <template v-if="stepActive === 0">
+          <div class="part-header">
+            {{ tl('chooseConnectorType') }}
+          </div>
+          <el-row>
+            <el-col :span="24">
+              <el-radio-group v-model="chosenConnectorType">
+                <el-radio v-for="item in connectorType" :key="item" :label="item" border>
+                  <img
+                    height="80"
+                    width="80"
+                    :src="require(`@/assets/img/${item}.png`)"
+                    :alt="item"
+                  />
+                  <div>
+                    <div class="title">{{ String(item).toUpperCase() }}</div>
+                  </div>
+                </el-radio>
+              </el-radio-group>
+            </el-col>
+          </el-row>
+        </template>
+        <template v-if="stepActive === 1">
+          <connector-mqtt-config
+            v-model="connectorData"
+            v-model:tls="connectorTLS"
+          ></connector-mqtt-config>
+        </template>
+      </el-row>
+      <el-row class="config-btn">
+        <el-button
+          size="small"
+          type="primary"
+          v-if="stepActive === 1"
+          :loading="submitLoading"
+          @click="submitCreateConnector"
+          >{{ $t('Base.create') }}</el-button
+        >
+        <el-button
+          type="primary"
+          size="small"
+          @click="++stepActive"
+          v-if="stepActive < 1"
+          :disabled="submitLoading"
+          >{{ $t('Base.nextStep') }}</el-button
+        >
+        <el-button
+          size="small"
+          @click="--stepActive"
+          v-if="stepActive > 0"
+          :disabled="submitLoading"
+          >{{ $t('Base.backStep') }}</el-button
+        >
+        <el-button
+          size="small"
+          v-if="stepActive === 0"
+          @click="$router.push({ name: 'bridge-connector' })"
+          >{{ $t('Base.cancel') }}</el-button
+        >
+      </el-row>
+      <div></div>
     </div>
-
-    <el-row>
-      <el-col :span="12">
-        <el-steps :active="stepActive" finish-status="success">
-          <el-step :title="tl('connectorType')"> </el-step>
-          <el-step :title="tl('configuration')"></el-step>
-        </el-steps>
-      </el-col>
-    </el-row>
-    <el-row class="config-body">
-      <template v-if="stepActive === 0">
-        <div class="part-header">
-          {{ tl('chooseConnectorType') }}
-        </div>
-        <el-row>
-          <el-col :span="24">
-            <el-radio-group v-model="chosenConnectorType">
-              <el-radio v-for="item in connectorType" :key="item" :label="item" border>
-                <img
-                  height="80"
-                  width="80"
-                  :src="require(`@/assets/img/${item}.png`)"
-                  :alt="item"
-                />
-                <div>
-                  <div class="title">{{ String(item).toUpperCase() }}</div>
-                </div>
-              </el-radio>
-            </el-radio-group>
-          </el-col>
-        </el-row>
-      </template>
-      <template v-if="stepActive === 1">
-        <connector-mqtt-config
-          v-model="connectorData"
-          v-model:tls="connectorTLS"
-        ></connector-mqtt-config>
-      </template>
-    </el-row>
-    <el-row class="config-btn">
-      <el-button
-        size="small"
-        type="primary"
-        v-if="stepActive === 1"
-        :loading="submitLoading"
-        @click="submitCreateConnector"
-        >{{ $t('Base.create') }}</el-button
-      >
-      <el-button
-        type="primary"
-        size="small"
-        @click="++stepActive"
-        v-if="stepActive < 1"
-        :disabled="submitLoading"
-        >{{ $t('Base.nextStep') }}</el-button
-      >
-      <el-button
-        size="small"
-        @click="--stepActive"
-        v-if="stepActive > 0"
-        :disabled="submitLoading"
-        >{{ $t('Base.backStep') }}</el-button
-      >
-      <el-button
-        size="small"
-        v-if="stepActive === 0"
-        @click="$router.push({ name: 'bridge-connector' })"
-        >{{ $t('Base.cancel') }}</el-button
-      >
-    </el-row>
-    <div></div>
   </div>
 </template>
 
