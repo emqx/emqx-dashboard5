a9e1a12701a842319814187f122fabebe16fef9f
Kinplemelon <wujiu529@gmail.com>
2022-03-14 18:18:53 +0800

    feat(exhook): add desc to some labels in form

--- src/views/Exhook/components/ExhookForm.vue
+++ src/views/Exhook/components/ExhookForm.vue
@@ -21,25 +21,41 @@
       <p class="block-title form-sub-block-title">{{ tl('connectionParameters') }}</p>
       <el-row :gutter="24">
         <el-col :span="12">
-          <el-form-item required prop="url" label="URL">
+          <el-form-item required prop="url">
+            <template #label>
+              <label>URL</label>
+              <InfoTooltip :content="tl('urlDesc')" />
+            </template>
             <el-input v-model="formData.url" />
           </el-form-item>
         </el-col>
       </el-row>
       <el-row :gutter="24">
         <el-col :span="12">
-          <el-form-item required prop="pool_size" label="Pool size">
+          <el-form-item required prop="pool_size">
+            <template #label>
+              <label>Pool size</label>
+              <InfoTooltip :content="tl('poolSizeDesc')" />
+            </template>
             <el-input v-model.number="formData.pool_size" />
           </el-form-item>
         </el-col>
         <el-col :span="12">
-          <el-form-item required prop="request_timeout" :label="tl('requestTimeOut')">
+          <el-form-item required prop="request_timeout">
+            <template #label>
+              <label>{{ tl('requestTimeOut') }}</label>
+              <InfoTooltip :content="tl('requestTimeOutDesc')" />
+            </template>
             <InputWithUnit v-model="formData.request_timeout" :units="timeoutUnits" />
           </el-form-item>
         </el-col>
 
         <el-col :span="12">
-          <el-form-item required prop="failed_action" :label="tl('failedAction')">
+          <el-form-item required prop="failed_action">
+            <template #label>
+              <label>{{ tl('failedAction') }}</label>
+              <InfoTooltip :content="tl('failedActionDesc')" />
+            </template>
             <el-select v-model="formData.failed_action">
               <el-option :label="ExhookFailedAction.Deny" :value="ExhookFailedAction.Deny" />
               <el-option :label="ExhookFailedAction.Ignore" :value="ExhookFailedAction.Ignore" />
@@ -47,7 +63,11 @@
           </el-form-item>
         </el-col>
         <el-col :span="12">
-          <el-form-item required prop="auto_reconnect" :label="tl('autoReconnect')">
+          <el-form-item required prop="auto_reconnect">
+            <template #label>
+              <label>{{ tl('autoReconnect') }}</label>
+              <InfoTooltip :content="tl('autoReconnectDesc')" />
+            </template>
             <el-select v-model="formData.auto_reconnect">
               <el-option label="true" :value="true" />
               <el-option label="false" :value="false" />
@@ -84,6 +104,7 @@ import { Exhook, ExhookFormForCreate } from '@/types/systemModule'
 import { useI18n } from 'vue-i18n'
 import useFormRules from '@/hooks/useFormRules'
 import InputWithUnit from '@/components/InputWithUnit.vue'
+import InfoTooltip from '@/components/InfoTooltip.vue'
 
 const props = defineProps({
   modelValue: {
