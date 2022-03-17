351b04bb71c87160a873e4394df769498b18ab68
YuShifan <894402575bt@gmail.com>
2022-03-17 17:01:21 +0800

    feat(config): add tooltip for read-only config item

--- src/i18n/BasicConfig.js
+++ src/i18n/BasicConfig.js
@@ -47,4 +47,8 @@ export default {
     zh: '此操作将移除 {name}，是否继续？',
     en: 'This will remove {name}, continue?',
   },
+  readOnlyTip: {
+    zh: '此配置项不支持热配置，请到配置文件内修改并重启服务',
+    en: 'This configuration item does not support hot configuration, please go to the configuration file to modify and restart the service',
+  },
 }
