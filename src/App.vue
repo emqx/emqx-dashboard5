3c620446844da2dcea7222e2dd3e898deeb57a12
ysfscream <894402575bt@gmail.com>
2022-03-30 16:56:57 +0800

    refactor(scss): add theme files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+import { useStore } from 'vuex'
+
+const store = useStore()
+const setTheme = () => {
+  let currentTheme = store.state.theme
+  const bodyTag: HTMLBodyElement | null = document.querySelector('body')
+  if (!bodyTag) {
+    return
+  }
+  bodyTag.className = currentTheme
+}
+setTheme()
+</script>
 
 <style lang="scss"></style>
