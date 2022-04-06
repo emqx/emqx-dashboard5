104d95ca625520752dddba40fc866522e4047a18
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-04-06 07:35:01 +0200

    fix: do not use curry brackets in messages
    Co-authored-by: YuShifan <894402575bt@gmail.com>

--- src/i18n/Plugins.js
+++ src/i18n/Plugins.js
@@ -84,8 +84,8 @@ export default {
     en: 'Upload file',
   },
   uploadTip: {
-    zh: '您可以在这里上传安装 .tar.gz 格式的 plugin 安装包，或用其他方法将插件包上传到 EMQX 所在服务器的 {data_data}/plugins/ 目录。随后到插件列表进行安装。',
-    en: 'You can upload and install a plugin .tar.gz package from here, or upload the package to {data_dir}/plugins/, then you should be able to see it in the list and install from there.',
+    zh: '您可以在这里上传安装 .tar.gz 格式的 plugin 安装包，或用其他方法将插件包上传到 EMQX 所在服务器的 data_data/plugins/ 目录。随后到插件列表进行安装。',
+    en: 'You can upload and install a plugin .tar.gz package from here, or upload the package to data_dir/plugins/, then you should be able to see it in the list and install from there.',
   },
   basicInformation: {
     zh: '基本信息',
