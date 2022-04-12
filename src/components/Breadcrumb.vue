636a6811bccdd01aeee67340d622b2fb3de1664b
ysfscream <894402575bt@gmail.com>
2022-04-12 15:02:44 +0800

    feat(breadcrumb): add breadcrumb component

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-breadcrumb class="breadcrumb" :separator-icon="ArrowRight">
+    <template v-for="item in items">
+      <el-breadcrumb-item v-if="item.path" :key="item.name" :to="{ path: item.path }">
+        {{ item.name }}
+      </el-breadcrumb-item>
+      <el-breadcrumb-item v-else :key="item.name">
+        {{ item.name }}
+      </el-breadcrumb-item>
+    </template>
+  </el-breadcrumb>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineProps, PropType } from 'vue'
+defineComponent({
+  name: 'Breadcrumb',
+})
+</script>
+
+<script lang="ts" setup>
+import { ArrowRight } from '@element-plus/icons-vue'
+
+interface item {
+  path?: string
+  name: string
+}
+
+defineProps({
+  items: {
+    required: true,
+    type: Array as PropType<item[]>,
+  },
+})
+</script>
+
+<style lang="scss">
+.breadcrumb {
+  margin-top: 18px;
+  margin-bottom: 24px;
+  .el-breadcrumb__item {
+    font-size: 18px;
+  }
+  .el-breadcrumb__inner.is-link {
+    color: var(--color-title-primary);
+    &:hover {
+      color: var(--color-primary);
+    }
+  }
+}
+</style>
