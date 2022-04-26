ee4e4fc817ca3eefa3b7b9a0d5925df63b7b75fb
ysfscream <894402575bt@gmail.com>
2022-04-26 16:58:10 +0800

    refactor(login): refactor login page

--- src/common/docLinks.ts
+++ src/common/docLinks.ts
@@ -5,7 +5,7 @@ export type DocMap = Record<DocKey, string>
 export default (lang: string): DocMap => {
   return {
     home: lang === 'en' ? 'https://www.emqx.io/' : `https://www.emqx.io/${lang}`,
-    cloud: `https://www.emqx.com/${lang}/cloud`,
+    cloud: `https://www.emqx.com/${lang}/cloud?utm_source=emqx-dashboard&utm_medium=referral&utm_campaign=dashboard-to-cloud`,
     applyLicense: `https://www.emqx.com/${lang}/apply-licenses/emqx`,
   }
 }
