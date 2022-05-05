7f68da26b97a36fb950a4a2724a1ccf56987fdae
Kinplemelon <wujiu529@gmail.com>
2022-05-05 14:43:11 +0800

    refactor(TLSConfig&listener): refactor some logic and use component in listener dialog

--- src/components/TLSConfig/TLSEnableConfig.vue
+++ src/components/TLSConfig/TLSEnableConfig.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="TLS-enable-config">
-    <el-form-item label="SNI">
+    <el-form-item label="SNI" v-if="showSni">
       <el-input class="TLS-input" v-model="record.server_name_indication" />
     </el-form-item>
     <el-form-item>
@@ -9,11 +9,14 @@
         <InfoTooltip :content="$t('Base.tlsConfigItemDesc', { file: 'TLS Cert' })" />
       </template>
       <TextareaWithUploader
+        v-if="!isEdit || !record.certfile || openResetMap.certfile"
         class="TLS-input"
         v-model="record.certfile"
         :allow-extensions="fileAllowExtensions"
         :placeholder="$t('Base.certPlaceholder')"
+        @vnode-mounted="editConfigItem('certfile')"
       />
+      <ConfigItemDataLook v-else :value="record.certfile" @reset="editConfigItem('certfile')" />
     </el-form-item>
     <el-form-item>
       <template #label>
@@ -21,11 +24,14 @@
         <InfoTooltip :content="$t('Base.tlsConfigItemDesc', { file: 'TLS Key' })" />
       </template>
       <TextareaWithUploader
+        v-if="!isEdit || !record.keyfile || openResetMap.keyfile"
         class="TLS-input"
         v-model="record.keyfile"
         :allow-extensions="fileAllowExtensions"
         :placeholder="$t('Base.keyFilePlaceholder')"
+        @vnode-mounted="editConfigItem('keyfile')"
       />
+      <ConfigItemDataLook v-else :value="record.keyfile" @reset="editConfigItem('keyfile')" />
     </el-form-item>
     <el-form-item>
       <template #label>
@@ -33,11 +39,14 @@
         <InfoTooltip :content="$t('Base.tlsConfigItemDesc', { file: 'CA Cert' })" />
       </template>
       <TextareaWithUploader
+        v-if="!isEdit || !record.cacertfile || openResetMap.cacertfile"
         class="TLS-input"
         v-model="record.cacertfile"
         :allow-extensions="fileAllowExtensions"
         :placeholder="$t('Base.certPlaceholder')"
+        @vnode-mounted="editConfigItem('cacertfile')"
       />
+      <ConfigItemDataLook v-else :value="record.cacertfile" @reset="editConfigItem('cacertfile')" />
     </el-form-item>
   </div>
 </template>
@@ -51,10 +60,22 @@ export default defineComponent({
 </script>
 
 <script setup lang="ts">
-import { defineProps, defineEmits, computed, PropType, WritableComputedRef } from 'vue'
+import {
+  defineProps,
+  defineEmits,
+  computed,
+  PropType,
+  WritableComputedRef,
+  Ref,
+  ref,
+  watch,
+} from 'vue'
 import InfoTooltip from '@/components/InfoTooltip.vue'
 import TextareaWithUploader from '../TextareaWithUploader.vue'
 import { SSL } from '@/types/common'
+import ConfigItemDataLook from './ConfigItemDataLook.vue'
+
+type ConfigItemKey = 'certfile' | 'keyfile' | 'cacertfile'
 
 const fileAllowExtensions = ['crt', 'key', 'pem', 'jks', 'der', 'cer', 'pfx']
 
@@ -62,6 +83,13 @@ const props = defineProps({
   modelValue: {
     type: Object as PropType<SSL>,
     default: () => ({}),
+  },
+  isEdit: {
+    type: Boolean,
+  },
+  showSni: {
+    type: Boolean,
+    default: true,
   },
 })
 
@@ -75,4 +103,15 @@ const record: WritableComputedRef<SSL> = computed({
     emit('update:modelValue', val)
   },
 })
+
+const openResetMap: Ref<Record<ConfigItemKey, boolean>> = ref({
+  certfile: false,
+  keyfile: false,
+  cacertfile: false,
+})
+
+const editConfigItem = (key: ConfigItemKey) => {
+  record.value[key] = ''
+  openResetMap.value[key] = true
+}
 </script>
