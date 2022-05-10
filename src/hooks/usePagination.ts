6b76a7fba0b951a928fad2977de63b95bfda4ece
Kinplemelon <wujiu529@gmail.com>
2022-05-10 15:50:58 +0800

    feat(retainer): add pagination

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,9 +1,32 @@
-import { ref } from 'vue'
+import { PageData } from '@/types/common'
+import { computed, ref, Ref, WritableComputedRef } from 'vue'
 
-export default () => {
+export default (): {
+  page: Ref<number>
+  limit: Ref<number>
+  count: Ref<number>
+  pageParams: WritableComputedRef<PageData>
+  resetPageNum: (tableData: Array<unknown>, pageNum: number) => number
+} => {
   const page = ref(1)
   const limit = ref(20)
   const count = ref(0)
+
+  const pageParams = computed({
+    get() {
+      return {
+        page: page.value,
+        limit: limit.value,
+        count: count.value,
+      }
+    },
+    set(data: PageData) {
+      page.value = data.page
+      limit.value = data.limit
+      count.value = data.count
+    },
+  })
+
   /**
    * reset
    */
@@ -13,10 +36,12 @@ export default () => {
     }
     return pageNum
   }
+
   return {
     page,
     limit,
     count,
+    pageParams,
     resetPageNum,
   }
 }
