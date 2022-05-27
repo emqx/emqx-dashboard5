2805e1632a35f33eb4838eebac5449a0bbaa672f
Kinplemelon <wujiu529@gmail.com>
2022-05-27 17:26:04 +0800

    style(bridge): style error

--- src/views/RuleEngine/Bridge/BridgeMqttConfig.vue
+++ src/views/RuleEngine/Bridge/BridgeMqttConfig.vue
@@ -124,7 +124,7 @@
           <div>
             <p class="part-sub-title">{{ tl('bridgeSinkFromLabel') }}</p>
             <el-row :gutter="30">
-              <el-col :span="10">
+              <el-col :span="24">
                 <el-radio-group
                   v-model="isForwardFromLocalTopic"
                   size="large"
