2093f21220293ef0a7a374d166a307d85b3e6514
Kinplemelon <wujiu529@gmail.com>
2022-07-25 15:46:01 +0800

    refactor(bridge&rule): adjusted some styles & add action metrics in rule detail page

--- src/views/RuleEngine/Bridge/Components/BridgeItemOverview.vue
+++ src/views/RuleEngine/Bridge/Components/BridgeItemOverview.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="resource-item-overview">
-    <el-card class="app-card detail-sub-card">
+    <div class="overview-sub-block">
       <div class="card-hd">
         <h2 class="block-title">{{ tl('executionStatistics') }}</h2>
         <el-tooltip effect="dark" :content="tl('resetStatistics')" placement="top-start">
@@ -8,30 +8,36 @@
         </el-tooltip>
       </div>
       <!-- <p class="card-sub-desc">{{ tl('lastResetTime') }}: TODO:</p> -->
-      <el-row class="rule-statistic">
+      <el-row class="rule-statistic" :gutter="28">
         <el-col :span="6">
-          <p class="statistic-label">{{ tl('SuccessNum') }}</p>
-          <p class="statistic-num">{{ formatNumber(bridgeMsg?.metrics?.success) }}</p>
+          <el-card class="success-bg">
+            <p class="statistic-label">{{ tl('SuccessNum') }}</p>
+            <p class="statistic-num">{{ formatNumber(bridgeMsg?.metrics?.success) }}</p>
+          </el-card>
         </el-col>
         <el-col :span="6">
-          <p class="statistic-label">{{ tl('ErrNum') }}</p>
-          <p class="statistic-num">{{ formatNumber(bridgeMsg?.metrics?.failed) }}</p>
+          <el-card class="failed-bg">
+            <p class="statistic-label">{{ tl('ErrNum') }}</p>
+            <p class="statistic-num">{{ formatNumber(bridgeMsg?.metrics?.failed) }}</p>
+          </el-card>
         </el-col>
         <el-col :span="6">
-          <p class="statistic-label">{{ tl('speedNow') }}</p>
-          <p class="statistic-num">
-            <span>{{ formatNumber(bridgeMsg?.metrics?.rate) }}</span>
-            <span class="unit">msg/s</span>
-          </p>
+          <el-card class="rate-bg">
+            <p class="statistic-label">{{ tl('speedNow') }}</p>
+            <p class="statistic-num">
+              <span>{{ formatNumber(bridgeMsg?.metrics?.rate) }}</span>
+              <span class="unit">msg/s</span>
+            </p>
+          </el-card>
         </el-col>
       </el-row>
-    </el-card>
-    <el-card class="app-card detail-sub-card">
+    </div>
+    <div class="overview-sub-block">
       <div class="card-hd">
         <h6 class="block-title">{{ tl('nodeStatus') }}</h6>
       </div>
       <p class="card-sub-desc">{{ tl('nodeStatusBridgeDesc') }}</p>
-      <el-table :data="nodeStatusTableData" class="shadow-none">
+      <el-table :data="nodeStatusTableData">
         <el-table-column prop="node" :label="tl('name')" />
         <el-table-column prop="metrics.success" :label="tl('SuccessNum')" />
         <el-table-column prop="metrics.failed" :label="tl('ErrNum')" />
@@ -53,7 +59,7 @@
           </template>
         </el-table-column>
       </el-table>
-    </el-card>
+    </div>
   </div>
 </template>
 
