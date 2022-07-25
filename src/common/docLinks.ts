4f1ab33678959d2b1019d45af4706b0f28ff4a8e
Kinplemelon <wujiu529@gmail.com>
2022-07-25 11:04:42 +0800

    refactor(doc): link to cloud

--- src/common/docLinks.ts
+++ src/common/docLinks.ts
@@ -9,6 +9,6 @@ export default (lang: string): DocMap => {
     sqlTest: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-get-started.html#%E6%B5%8B%E8%AF%95-sql-%E8%AF%AD%E5%8F%A5`,
     bridgePayload: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/data-bridge-webhook.html#%E7%A4%BA%E4%BE%8B-%E4%BD%BF%E7%94%A8%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6%E5%88%9B%E5%BB%BA-webhook`,
     home: lang === 'en' ? 'https://www.emqx.io/' : `https://www.emqx.io/${lang}`,
-    cloud: `https://www.emqx.com/${lang}/cloud?utm_source=emqx-dashboard&utm_medium=referral&utm_campaign=dashboard-to-cloud`,
+    cloud: `https://www.emqx.com/${lang}/signup?utm_source=emqx-dashboard&utm_medium=referral&utm_campaign=dashboard-to-cloud&continue=https%3A%2F%2Fcloud-intl.emqx.com%2Fconsole%2F`,
   }
 }
