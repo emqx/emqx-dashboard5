17abff225ba032efe61bdb0f19f0d5868e9fa85b
Kinplemelon <wujiu529@gmail.com>
2022-08-10 11:40:45 +0800

    feat(bridge): support influx DB v2 & UDP

--- src/views/RuleEngine/Bridge/Components/UsingSchemaBridgeConfig.vue
+++ src/views/RuleEngine/Bridge/Components/UsingSchemaBridgeConfig.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <schema-form
+      v-if="getRefKey"
       ref="formCom"
       type="bridge"
       :need-footer="false"
       :need-record="!bridgeRecord"
       :form="bridgeRecord"
       :schema-file-path="`static/bridge-api-${store.state.lang}.json`"
-      :according-to="{ ref: '#/components/schemas/bridge_influxdb.post_api_v1' }"
+      :according-to="{ ref: `#/components/schemas/${getRefKey}` }"
       :btn-loading="saveLoading"
     >
     </schema-form>
@@ -15,14 +16,26 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineExpose, defineProps, computed, defineEmits } from 'vue'
+import { ref, defineExpose, defineProps, computed, defineEmits, PropType } from 'vue'
 import { useStore } from 'vuex'
 import SchemaForm from '@/components/SchemaForm'
+import { BridgeType } from '@/types/enum'
+
+type UseSchemaBridgeType = Exclude<BridgeType, BridgeType.MQTT | BridgeType.Webhook>
+
+const typeRefKeyMap: Record<UseSchemaBridgeType, string> = {
+  [BridgeType.InfluxDBV1]: `bridge_influxdb.post_api_v1`,
+  [BridgeType.InfluxDBV2]: `bridge_influxdb.post_api_v2`,
+  [BridgeType.InfluxDBUPD]: `bridge_influxdb.post_udp`,
+}
 
 const props = defineProps({
   modelValue: {
     type: Object,
-    default: () => ({}),
+    default: undefined,
+  },
+  type: {
+    type: String as PropType<UseSchemaBridgeType>,
   },
 })
 const emit = defineEmits(['update:modelValue'])
@@ -32,6 +45,13 @@ const store = useStore()
 const saveLoading = ref(false)
 
 const formCom = ref()
+
+const getRefKey = computed(() => {
+  if (!props.type) {
+    return
+  }
+  return typeRefKeyMap[props.type] || undefined
+})
 
 const bridgeRecord = computed({
   get() {
