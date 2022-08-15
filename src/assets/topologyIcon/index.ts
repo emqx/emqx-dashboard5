cf3645f9f4f3f6b1287a0119b76e19b1151ed0b6
Kinplemelon <wujiu529@gmail.com>
2022-08-15 17:44:08 +0800

    fix(flow): add type for influxDB & mysql

--- src/assets/topologyIcon/index.ts
+++ src/assets/topologyIcon/index.ts
@@ -1,5 +1,6 @@
-import bridgeWebhook from './bridge-webhook.png'
-import bridgeMQTT from './bridge-mqtt.png'
+import { BridgeType } from '@/types/enum'
+import bridgeWebhook from '@/assets/img/webhook.png'
+import bridgeMQTT from '@/assets/img/mqtt.png'
 import console from './console.png'
 import event from './event.png'
 import republish from './republish.png'
@@ -7,8 +8,8 @@ import rule from './rule.png'
 import topic from './topic.png'
 
 const iconMap: Record<string, SVGAElement> = {
-  'bridge-webhook': bridgeWebhook,
-  'bridge-mqtt': bridgeMQTT,
+  [`bridge-${BridgeType.Webhook}`]: bridgeWebhook,
+  [`bridge-${BridgeType.MQTT}`]: bridgeMQTT,
   console,
   event,
   republish,
