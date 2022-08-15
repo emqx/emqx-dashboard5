25c834a830f20d8a9e2af3b12d444984dc6b51b8
Kinplemelon <wujiu529@gmail.com>
2022-08-15 18:14:05 +0800

    feat(flow): add icon for influxDB and mysql

--- src/assets/topologyIcon/index.ts
+++ src/assets/topologyIcon/index.ts
@@ -1,6 +1,8 @@
 import { BridgeType } from '@/types/enum'
 import bridgeWebhook from '@/assets/img/webhook.png'
 import bridgeMQTT from '@/assets/img/mqtt.png'
+import bridgeInfluxDB from '@/assets/img/influxdb.png'
+import bridgeMySQL from '@/assets/img/mysql.png'
 import console from './console.png'
 import event from './event.png'
 import republish from './republish.png'
@@ -10,6 +12,10 @@ import topic from './topic.png'
 const iconMap: Record<string, SVGAElement> = {
   [`bridge-${BridgeType.Webhook}`]: bridgeWebhook,
   [`bridge-${BridgeType.MQTT}`]: bridgeMQTT,
+  [`bridge-${BridgeType.InfluxDBV1}`]: bridgeInfluxDB,
+  [`bridge-${BridgeType.InfluxDBV2}`]: bridgeInfluxDB,
+  [`bridge-${BridgeType.InfluxDBUPD}`]: bridgeInfluxDB,
+  [`bridge-${BridgeType.MySQL}`]: bridgeMySQL,
   console,
   event,
   republish,
