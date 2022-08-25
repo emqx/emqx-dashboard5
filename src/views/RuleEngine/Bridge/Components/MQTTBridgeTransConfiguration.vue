bb46353c9f2b7e42d516b480aea53240185fbfd3
Kinplemelon <wujiu529@gmail.com>
2022-08-25 16:46:32 +0800

    refactor(bridge): layout of mqtt bridge form

--- src/views/RuleEngine/Bridge/Components/MQTTBridgeTransConfiguration.vue
+++ src/views/RuleEngine/Bridge/Components/MQTTBridgeTransConfiguration.vue
@@ -1,37 +1,28 @@
 <template>
-  <el-row :gutter="26">
-    <el-col :span="12">
-      <el-form-item :prop="getProp('topic')">
-        <template #label>
-          <label>{{ isIngress ? tl('localTopic') : tl('remoteTopic') }}</label>
-          <!-- TODO:TODO:TODO:TODO: -->
-          <InfoTooltip :content="tl('remoteTopicDesc')" />
-        </template>
-        <el-input
-          v-model="config.topic"
-          :placeholder="isIngress ? tl('remoteTopicPlaceholder') : ''"
-        />
-      </el-form-item>
-    </el-col>
-    <el-col :span="6">
-      <el-form-item label="QoS">
-        <el-select v-model="config.qos">
-          <el-option v-for="qos in QoSOptions" :key="qos" :value="qos" />
-        </el-select>
-      </el-form-item>
-    </el-col>
-    <el-col :span="6">
-      <el-form-item label="Retain">
-        <el-checkbox :label="'Retain'" border v-model="config.retain" />
-      </el-form-item>
-    </el-col>
-  </el-row>
+  <el-form-item :prop="getProp('topic')">
+    <template #label>
+      <!-- <label>{{ isIngress ? tl('localTopic') : tl('remoteTopic') }}</label> -->
+      <label>{{ t('Base.topic') }}</label>
+      <!-- TODO:TODO:TODO:TODO: -->
+      <InfoTooltip :content="tl('remoteTopicDesc')" />
+    </template>
+    <el-input v-model="config.topic" />
+    <!-- :placeholder="isIngress ? tl('remoteTopicPlaceholder') : ''" -->
+  </el-form-item>
+  <el-form-item label="QoS">
+    <el-select v-model="config.qos">
+      <el-option v-for="qos in QoSOptions" :key="qos" :value="qos" />
+    </el-select>
+  </el-form-item>
+  <el-form-item label="Retain">
+    <el-checkbox :label="'Retain'" border v-model="config.retain" />
+  </el-form-item>
   <el-row :gutter="26">
     <el-col :span="24">
       <el-form-item>
         <template #label>
           <label>{{ tl('payload') }}</label>
-          <i18n-t class="payload-desc" keypath="RuleEngine.payloadDesc" tag="p">
+          <i18n-t class="payload-desc" keypath="RuleEngine.payloadDesc" tag="p" scope="global">
             <a :href="docMap.bridgePayload" target="_blank">{{ tl('payloadTempSyntax') }}</a>
           </i18n-t>
         </template>
@@ -56,19 +47,13 @@ import InfoTooltip from '@/components/InfoTooltip.vue'
 import Monaco from '@/components/Monaco.vue'
 import useDocLink from '@/hooks/useDocLink'
 import useI18nTl from '@/hooks/useI18nTl'
-import { MQTTBridgeDirection, QoSLevel } from '@/types/enum'
+import { MQTTBridgeDirection } from '@/types/enum'
 import { computed, defineEmits, defineProps, PropType } from 'vue'
-
-interface TransConfiguration {
-  topic: string
-  qos: QoSLevel
-  payload: string
-  retain: boolean
-}
+import { MQTTBridgeTransConfiguration } from '@/types/rule'
 
 const props = defineProps({
   modelValue: {
-    type: Object as PropType<TransConfiguration>,
+    type: Object as PropType<MQTTBridgeTransConfiguration>,
     required: true,
   },
   disabled: {
@@ -84,7 +69,7 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue'])
 
-const { tl } = useI18nTl('RuleEngine')
+const { tl, t } = useI18nTl('RuleEngine')
 const { docMap } = useDocLink()
 
 const config = computed({
@@ -96,7 +81,12 @@ const config = computed({
   },
 })
 
-const isIngress = computed(() => props.direction === MQTTBridgeDirection.In)
-
 const getProp = (key: string) => (props.path ? '' : `${props.path}.${key}`)
 </script>
+
+<style class="scss">
+.payload-desc {
+  margin: 8px 0;
+  color: var(--el-text-color-secondary);
+}
+</style>
