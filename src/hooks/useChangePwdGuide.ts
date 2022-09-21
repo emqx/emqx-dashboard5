ace525b38d46cdd1d554f160130a99d0749635d8
Kinplemelon <wujiu529@gmail.com>
2022-09-21 18:35:17 +0800

    fix(user): patch holes which user can do not change default pwd

--- src/hooks/useChangePwdGuide.ts
+++ src/hooks/useChangePwdGuide.ts
@@ -4,11 +4,46 @@ import { ElMessageBox } from 'element-plus'
 import { useRouter, useRoute } from 'vue-router'
 import { User } from '@/types/systemModule'
 import useI18nTl from './useI18nTl'
+import {
+  RouteLocationNormalized,
+  NavigationGuardNext,
+  onBeforeRouteLeave,
+  onBeforeRouteUpdate,
+} from 'vue-router'
+
+export const useRouteGuardForChangeDefaultPwd = () => {
+  const { state } = useStore()
+
+  const preventLeaveWithoutChangeDefaultPwd = (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext,
+  ) => {
+    const { name = '', params = {} } = to
+
+    if (
+      state.user.isUsingDefaultPwd &&
+      // For stop infinite loop
+      !(name === 'users' && params.forChangeDefaultPwd === 'true')
+    ) {
+      ;(next as any)({
+        name: 'users',
+        params: { forChangeDefaultPwd: true },
+      })
+    } else {
+      next()
+    }
+  }
+
+  return { preventLeaveWithoutChangeDefaultPwd }
+}
 
 export default (): void => {
   const store = useStore()
   const router = useRouter()
   const { tl } = useI18nTl('Base')
+
+  let isMsgBoxClosed = false
 
   const isUsingDefaultPwd = computed(() => {
     return store.state.user.isUsingDefaultPwd
@@ -20,7 +55,11 @@ export default (): void => {
       message: tl('defaultPwdTip'),
       confirmButtonText: 'OK',
       customClass: 'default-pwd-tip',
+      closeOnClickModal: false,
+      closeOnPressEscape: false,
+      closeOnHashChange: false,
       callback: () => {
+        isMsgBoxClosed = true
         router.push({
           name: 'users',
           params: {
@@ -36,6 +75,21 @@ export default (): void => {
     if (isUsingDefaultPwd.value) {
       popupMessageBox()
     }
+  })
+
+  const { preventLeaveWithoutChangeDefaultPwd } = useRouteGuardForChangeDefaultPwd()
+  onBeforeRouteLeave((to, from, next) => {
+    if (isUsingDefaultPwd.value && !isMsgBoxClosed) {
+      ElMessageBox.close()
+    }
+    preventLeaveWithoutChangeDefaultPwd(to, from, next)
+  })
+
+  onBeforeRouteUpdate((to, from, next) => {
+    if (!isMsgBoxClosed && to.name === from.name && to.name === 'users') {
+      ElMessageBox.close()
+    }
+    next()
   })
 }
 
