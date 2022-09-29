89da0e94bfe809895f0ec84717f46f241758c9db
ysfscream <894402575bt@gmail.com>
2022-09-29 18:05:08 +0800

    fix(common): modify try cloud link

--- src/common/docLinks.ts
+++ src/common/docLinks.ts
@@ -16,13 +16,14 @@ type DocKey =
 export type DocMap = Record<DocKey, string>
 
 export default (lang: string): DocMap => {
+  const accountsLink = lang === 'zh' ? 'accounts-zh.emqx.com' : 'accounts.emqx.com'
   return {
     sqlGrammar: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-sql-grammar-and-examples.html`,
     sqlActionRepub: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-actions.html#%E6%B6%88%E6%81%AF%E9%87%8D%E5%8F%91%E5%B8%83-%E5%8A%A8%E4%BD%9C`,
     sqlTest: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-get-started.html#%E6%B5%8B%E8%AF%95-sql-%E8%AF%AD%E5%8F%A5`,
     bridgePayload: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/data-bridge-webhook.html#%E7%A4%BA%E4%BE%8B-%E4%BD%BF%E7%94%A8%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6%E5%88%9B%E5%BB%BA-webhook`,
     home: lang === 'en' ? 'https://www.emqx.io/' : `https://www.emqx.io/${lang}`,
-    cloud: `https://www.emqx.com/${lang}/signup?utm_source=emqx-dashboard&utm_medium=referral&utm_campaign=dashboard-to-cloud&continue=https%3A%2F%2Fcloud-intl.emqx.com%2Fconsole%2F`,
+    cloud: `https://${accountsLink}/signup?utm_source=emqx-dashboard&utm_medium=referral&utm_campaign=dashboard-to-cloud&continue=https%3A%2F%2Fcloud-intl.emqx.com%2Fconsole%2F`,
     // TODO: version
     documentation: `https://www.emqx.io/docs/${lang}/v5.0/`,
     forum: lang === 'en' ? `https://www.emqx.io/forum/` : `https://askemq.com/`,
