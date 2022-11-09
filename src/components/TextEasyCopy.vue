9a9f7b8488617c52d39027ff44a2c261023a4936
Kinplemelon <wujiu529@gmail.com>
2022-11-09 15:08:30 +0800

    feat(connection): supported copy client id in detail page

---
+++ src/components/TextEasyCopy.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="text-easy-copy" ref="textWrap" @click="handleClick">
+    <slot></slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import useCopy from '@/hooks/useCopy'
+
+const textWrap = ref()
+
+const { copyText } = useCopy()
+
+const handleClick = () => {
+  const text = textWrap.value.innerText
+  copyText(text)
+}
+</script>
+
+<style lang="scss">
+.text-easy-copy {
+  display: inline-block;
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 4px;
+  cursor: default;
+  &:hover {
+    background-color: var(--color-hover);
+  }
+}
+</style>
