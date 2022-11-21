9d986a5c62c3c5c5cf59c85e3d81908888dccb05
Kinplemelon <wujiu529@gmail.com>
2022-11-21 14:48:20 +0800

    feat(bridge): handle backslash and double quotes in field value on influx db

--- src/hooks/Rule/bridge/useInfluxdbLineProtocol.ts
+++ src/hooks/Rule/bridge/useInfluxdbLineProtocol.ts
@@ -1,5 +1,7 @@
 import { trim } from 'lodash'
-
+import useInfluxdbFieldsEditor, {
+  FieldValueType,
+} from '@/hooks/Rule/bridge/useInfluxdbFieldsEditor'
 export interface KeyValueItem {
   key: string
   value: string
@@ -17,7 +19,9 @@ export default (): {
   convertArrToMap: (arr: Array<KeyValueItem>) => Record<string, string>
   trimLineProtocol: (lineProtocol: string) => string
   escape: (str: string) => string
+  escapeFieldValue: (str: string) => string
   unescape: (str: string) => string
+  unescapeFieldValue: (str: string) => string
 } => {
   /* 
     # Syntax
@@ -32,15 +36,17 @@ export default (): {
     const SQL1 = 'weather,location=us-midwest,closeSea=true temperature=82,humidity=71 1465839830100400200'
     const SQL2 = `my\ Measurement,tag\ Key1=tag\ Value1,tag\ Key2=tag\ Value2,tagKey=🍭 fieldKey="\"string\" within a string",fieldKey2="Launch 🚀"`
    */
-  // FIXME: process field value
   // FIXME: process new row (\n)
   const measurementReg = /([^,\s]|(?<=\\),|(?<=\\)\s)+/
-  const keyNValueReg = /([^,\s]|(?<=\\),|(?<=\\)\s|(?<=\\)=)+/
+  const keyNValueReg = /([^,\s=]|(?<=\\),|(?<=\\)\s|(?<=\\)=)+/
+  const fieldValueReg = /(".+")|[^"\s,]+/
   const tagItemReg = new RegExp(
     `(?<tagKey>${keyNValueReg.source})=(?<tagValue>${keyNValueReg.source})`,
   )
   const tagPartReg = new RegExp(`(${tagItemReg.source},?)+`)
-  const fieldItemReg = new RegExp(`(?<fieldKey>${keyNValueReg.source})=(?<fieldValue>[^\\s,]+)`)
+  const fieldItemReg = new RegExp(
+    `(?<fieldKey>${keyNValueReg.source})=(?<fieldValue>${fieldValueReg.source})`,
+  )
   const fieldsPartReg = new RegExp(`(${fieldItemReg.source},?)+`)
   const timestampPart = /\d+/
 
@@ -103,19 +109,44 @@ export default (): {
   const specialLetterReg = /,|\s|=/g
   /**
    * add \ before comma, space and equals sign, for form to line protocol
+   * can escape tag key, tag value and field key
    * @param str key or value, not the total line protocol
    */
   const escape = (str: string): string => {
     return str.replace(specialLetterReg, (matched) => `\\${matched}`)
   }
+  const fieldValueEscapeReg = /"|\\/g
+  const escapeFieldValue = (str: string): string => {
+    if (judgeFieldValueType(str) === FieldValueType.String) {
+      return `${str[0]}${str
+        .slice(1, -1)
+        .replace(fieldValueEscapeReg, (matched) => `\\${matched}`)}${str.slice(-1)}`
+    }
+    return str
+  }
 
   const strPartAfterEscapeReg = /\\(=|\s|,)/g
   /**
    * remove \ before comma, space and equals sign, for line protocol to form
+   * can unescape tag key, tag value and field key
    * @param str key or value, not the total line protocol
    */
   const unescape = (str: string): string => {
     return str.replace(strPartAfterEscapeReg, (matched, p1) => p1)
+  }
+  const { judgeFieldValueType } = useInfluxdbFieldsEditor()
+
+  const fieldValueUnescapeReg = /\\("|\\)/g
+  /**
+   * backslash and double quotes
+   */
+  const unescapeFieldValue = (str: string): string => {
+    if (judgeFieldValueType(str) === FieldValueType.String) {
+      return `${str[0]}${str
+        .slice(1, -1)
+        .replace(fieldValueUnescapeReg, (matched, target) => target)}${str.slice(-1)}`
+    }
+    return str
   }
 
   const trimLineProtocol = (lineProtocol: string): string => trim(trim(lineProtocol), `\n`)
@@ -125,6 +156,8 @@ export default (): {
     convertArrToMap,
     trimLineProtocol,
     escape,
+    escapeFieldValue,
     unescape,
+    unescapeFieldValue,
   }
 }
--- src/views/RuleEngine/Bridge/Components/BridgeConfig/InfluxdbFieldsEditor.vue
+++ src/views/RuleEngine/Bridge/Components/BridgeConfig/InfluxdbFieldsEditor.vue
@@ -52,19 +52,13 @@ import { ref, computed, Ref, defineComponent, watch } from 'vue'
 import { isPlainObject, cloneDeep, isEqual } from 'lodash'
 import useI18nTl from '@/hooks/useI18nTl'
 import BooleanSelect from '@/components/BooleanSelect.vue'
+import useInfluxdbFieldsEditor, {
+  FieldValueType,
+} from '@/hooks/Rule/bridge/useInfluxdbFieldsEditor'
 
 enum State {
   OK = 0,
   Error,
-}
-enum FieldValueType {
-  Float,
-  Integer,
-  // TODO: confirm : seems just v2 with this type
-  UInteger,
-  String,
-  Boolean,
-  Placeholder,
 }
 type kvRow = {
   key: string
@@ -73,82 +67,8 @@ type kvRow = {
   state: State
 }
 
-const STRING_REGEX = /^".*"$/
-const BOOL_REGEX = /^((t|true)|(f|false))$/i
-const INT_REGEX = /^\d+i$/
-const U_INT_REGEX = /^\d+u$/
-const NUMBER_REGEX = /\d+(\.\d+)?/
-const SCIENTIFIC_NOTATION_REGEX = new RegExp(`${NUMBER_REGEX.source}e(\\+|-)\\d+`)
-const FLOAT_REGEX = new RegExp(`^${SCIENTIFIC_NOTATION_REGEX.source}|${NUMBER_REGEX.source}$`)
-const PLACEHOLDER_REGEX = /\$\{.+\}(i|u)?/
-const typeRegexMap: Record<FieldValueType, RegExp> = {
-  [FieldValueType.String]: STRING_REGEX,
-  [FieldValueType.Integer]: INT_REGEX,
-  [FieldValueType.UInteger]: U_INT_REGEX,
-  [FieldValueType.Boolean]: BOOL_REGEX,
-  [FieldValueType.Float]: FLOAT_REGEX,
-  [FieldValueType.Placeholder]: PLACEHOLDER_REGEX,
-}
-const typeRegexMapKeys: Array<FieldValueType> = Object.keys(typeRegexMap).map((key) => Number(key))
-const judgeFieldValueType = (str: string) => {
-  const ret = typeRegexMapKeys.find((type: FieldValueType) => {
-    return typeRegexMap[type].test(str)
-  })
-  return ret ?? undefined
-}
-
-const convertToRawValueByType = (valueInProtocolLine: string, type: FieldValueType) => {
-  switch (type) {
-    case FieldValueType.String:
-      if (STRING_REGEX.test(valueInProtocolLine)) {
-        return valueInProtocolLine.slice(1, -1)
-      }
-      break
-    case FieldValueType.Integer:
-      if (INT_REGEX.test(valueInProtocolLine)) {
-        return valueInProtocolLine.slice(0, -1)
-      }
-      break
-    case FieldValueType.UInteger:
-      if (U_INT_REGEX.test(valueInProtocolLine)) {
-        return valueInProtocolLine.slice(0, -1)
-      }
-      break
-    case FieldValueType.Boolean:
-      if (BOOL_REGEX.test(valueInProtocolLine)) {
-        return valueInProtocolLine[0].toLocaleLowerCase() === 't' ? true : false
-      }
-      break
-  }
-  // float and others, return value directly
-  return valueInProtocolLine
-}
-
-/**
- * convert raw value to value in line protocol
- */
-const handleValueByType = (value: string | boolean, type: FieldValueType) => {
-  switch (type) {
-    case FieldValueType.String:
-      return `"${value}"`
-    case FieldValueType.Integer:
-      if (Number.isNaN(Number(value))) {
-        throw new Error()
-      }
-      return `${value}i`
-    case FieldValueType.UInteger:
-      if (Number.isNaN(Number(value))) {
-        throw new Error()
-      }
-      return `${value}u`
-    case FieldValueType.Boolean:
-      return value
-    case FieldValueType.Float:
-      return value
-    default:
-      return value
-  }
-}
+const { judgeFieldValueType, convertToRawValueByType, handleValueByType } =
+  useInfluxdbFieldsEditor()
 
 export default defineComponent({
   name: 'KeyAndValueEditor',
