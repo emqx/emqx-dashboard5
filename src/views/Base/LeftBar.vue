99943bda83467dfe0ebda9d47081a0cb24786229
ysfscream <894402575bt@gmail.com>
2022-11-23 17:32:00 +0800

    fix(retained): move the retained message menu to bottom

--- src/views/Base/LeftBar.vue
+++ src/views/Base/LeftBar.vue
@@ -197,11 +197,6 @@ export default defineComponent({
         path: '/subscriptions',
       },
       {
-        title: 'retained',
-        icon: 'icon-retained',
-        path: '/retained',
-      },
-      {
         title: 'auth',
         icon: 'icon-authentication',
         c: authentication,
@@ -215,6 +210,11 @@ export default defineComponent({
         title: 'configuration',
         icon: 'icon-configuration',
         c: config,
+      },
+      {
+        title: 'retained',
+        icon: 'icon-retained',
+        path: '/retained',
       },
       {
         title: 'extensions',
