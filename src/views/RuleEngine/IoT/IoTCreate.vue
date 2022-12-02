b092d8a691e08f60616b9ae51ef2f53cbb38feef
Kinplemelon <wujiu529@gmail.com>
2022-12-02 16:44:32 +0800

    fix(rule): data bridge duplicate name is incorrect

--- src/views/RuleEngine/IoT/IoTCreate.vue
+++ src/views/RuleEngine/IoT/IoTCreate.vue
@@ -29,7 +29,7 @@ import { RuleItem } from '@/types/ruleengine'
 import { createRules, getRuleInfo } from '@/api/ruleengine'
 import { useRoute, useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
-import { createRandomString } from '@/common/tools'
+import { countDuplicationName, createRandomString } from '@/common/tools'
 import { DEFAULT_SELECT, DEFAULT_FROM } from '@/common/constants'
 import { useRuleUtils } from '@/hooks/Rule/topology/useRule'
 import useDataNotSaveConfirm from '@/hooks/useDataNotSaveConfirm'
@@ -63,8 +63,9 @@ const checkRuleClipStatus = async () => {
   if (route.query.action === 'copy' && route.query.target) {
     const ruleData = await getRuleInfo(route.query.target as string)
     const rule = pick(ruleData, ['sql', 'actions', 'description', 'actions'])
+
     if (rule) {
-      ruleValue.value = { ...(rule as RuleItem), id: `${ruleData.id}_duplication` }
+      ruleValue.value = { ...(rule as RuleItem), id: countDuplicationName(ruleData.id) }
     }
   }
 }
