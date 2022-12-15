1edac3721688be47520175506c7b8fd98eb20a14
Kinplemelon <wujiu529@gmail.com>
2022-12-15 14:50:15 +0800

    fix(schema form): check before set config form value

--- src/components/SchemaForm.tsx
+++ src/components/SchemaForm.tsx
@@ -171,7 +171,7 @@ const SchemaForm = defineComponent({
     watch(
       () => props.form,
       (value) => {
-        if (value) {
+        if (value && !_.isEqual(value, configForm.value)) {
           configForm.value = _.cloneDeep(value)
         }
       },
