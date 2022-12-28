57b2b4b338542aadb56d837b39ae06c04aaef0bd
Kinplemelon <wujiu529@gmail.com>
2022-12-28 17:04:46 +0800

    refactor(bridge): add back the Test Connection button

--- src/hooks/Rule/bridge/useBridgeDataHandler.ts
+++ src/hooks/Rule/bridge/useBridgeDataHandler.ts
@@ -1,10 +1,13 @@
-import { checkNOmitFromObj } from '@/common/tools'
+import { checkNOmitFromObj, utf8Encode } from '@/common/tools'
 import { BridgeType } from '@/types/enum'
 import { cloneDeep } from 'lodash'
+import useSSL from '@/hooks/useSSL'
 
 export default (): {
   handleBridgeDataBeforeSubmit: (bridgeData: any) => any
 } => {
+  const { handleSSLDataBeforeSubmit } = useSSL()
+
   const handleMQTTBridgeData = (bridgeData: any) => {
     const { egress, ingress } = bridgeData
     if (!egress.remote?.topic) {
@@ -15,10 +18,23 @@ export default (): {
     return bridgeData
   }
 
+  const handleWebhookBridgeData = (bridgeData: any) => {
+    if (bridgeData.body) {
+      bridgeData.body = utf8Encode(bridgeData.body)
+    }
+    return bridgeData
+  }
+
   const handleBridgeDataBeforeSubmit = (bridgeData: any): any => {
     let ret = cloneDeep(bridgeData)
+    if (ret.ssl) {
+      ret.ssl = handleSSLDataBeforeSubmit(ret.ssl)
+    }
     if (ret.type === BridgeType.MQTT) {
       ret = handleMQTTBridgeData(ret)
+    }
+    if (ret.type === BridgeType.Webhook) {
+      ret = handleWebhookBridgeData(ret)
     }
     return checkNOmitFromObj(ret)
   }
