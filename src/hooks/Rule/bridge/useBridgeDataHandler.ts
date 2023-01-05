e1101ef5c1ef32a788b0548f5a1599a39e277411
Kinplemelon <wujiu529@gmail.com>
2023-01-05 14:30:17 +0800

    fix(bridge data handler): return handleBridgeDataAfterLoaded func

--- src/hooks/Rule/bridge/useBridgeDataHandler.ts
+++ src/hooks/Rule/bridge/useBridgeDataHandler.ts
@@ -5,6 +5,7 @@ import useSSL from '@/hooks/useSSL'
 
 export default (): {
   handleBridgeDataBeforeSubmit: (bridgeData: any) => any
+  handleBridgeDataAfterLoaded: (bridgeData: any) => any
   handleBridgeDataForCopy: (bridgeData: any) => any
 } => {
   const { handleSSLDataBeforeSubmit } = useSSL()
@@ -59,6 +60,7 @@ export default (): {
 
   return {
     handleBridgeDataBeforeSubmit,
+    handleBridgeDataAfterLoaded,
     handleBridgeDataForCopy,
   }
 }
