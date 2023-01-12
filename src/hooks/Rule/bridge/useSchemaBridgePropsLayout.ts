71f0a45696907b7ac9415cc1136aed0e9f3ce867
Kinplemelon <wujiu529@gmail.com>
2023-01-12 17:41:06 +0800

    refactor(bridge): layout of some bridges

--- src/hooks/Rule/bridge/useSchemaBridgePropsLayout.ts
+++ src/hooks/Rule/bridge/useSchemaBridgePropsLayout.ts
@@ -29,7 +29,7 @@ export default (
     type: string
     enable: string
     local_topic: string
-    'connector.ssl': string
+    ssl: string
   }>
 } => {
   const createOrderObj = (keyArr: Array<string>, beginning: number) =>
@@ -39,14 +39,14 @@ export default (
     name: 0,
     ...createOrderObj(
       [
-        'worker_pool_size',
-        'health_check_interval',
-        'auto_restart_interval',
-        'max_queue_bytes',
-        'batch_size',
-        'batch_time',
-        'query_mode',
-        'async_inflight_window',
+        'resource_opts.worker_pool_size',
+        'resource_opts.health_check_interval',
+        'resource_opts.auto_restart_interval',
+        'resource_opts.max_queue_bytes',
+        'resource_opts.batch_size',
+        'resource_opts.batch_time',
+        'resource_opts.query_mode',
+        'resource_opts.async_inflight_window',
       ],
       99,
     ),
@@ -55,7 +55,7 @@ export default (
   const propsOrderTypeMap: Record<string, Record<string, number>> = {
     [BridgeType.MySQL]: {
       ...createOrderObj(
-        ['server', 'database', 'username', 'password', 'pool_size', 'auto_reconnect', 'sql', 'ssl'],
+        ['server', 'database', 'username', 'password', 'pool_size', 'auto_reconnect', 'ssl', 'sql'],
         1,
       ),
     },
@@ -70,6 +70,7 @@ export default (
           'sentinel',
           'pool_size',
           'auto_reconnect',
+          'ssl',
           'command_template',
         ],
         1,
@@ -95,6 +96,17 @@ export default (
           'collection',
           'ssl',
           'payload_template',
+          'topology.pool_size',
+          'topology.max_overflow',
+          'topology.overflow_ttl',
+          'topology.overflow_check_period',
+          'topology.local_threshold_ms',
+          'topology.connect_timeout_ms',
+          'topology.socket_timeout_ms',
+          'topology.server_selection_timeout_ms',
+          'topology.wait_queue_timeout_ms',
+          'topology.heartbeat_frequency_ms',
+          'topology.min_heartbeat_frequency_ms',
         ],
         1,
       ),
@@ -110,15 +122,19 @@ export default (
   })
 
   const typeColClassMap = {
-    [BridgeType.MySQL]: {},
+    [BridgeType.MySQL]: {
+      sql: 'dividing-line-below',
+    },
     [BridgeType.Redis]: {
-      command_template: 'custom-col-24',
+      command_template: 'custom-col-24 dividing-line-below',
     },
     [BridgeType.GCP]: {
-      payload_template: 'custom-col-24',
-      service_account_json: 'custom-col-24',
+      payload_template: 'custom-col-24  dividing-line-above',
+      service_account_json: 'custom-col-24 dividing-line-below',
     },
-    [BridgeType.MongoDB]: {},
+    [BridgeType.MongoDB]: {
+      payload_template: 'dividing-line-below',
+    },
   }
 
   const { syncEtcFieldsClassMap } = useSyncConfiguration(bridgeRecord)
@@ -131,7 +147,7 @@ export default (
       type: 'col-hidden',
       enable: 'col-hidden',
       local_topic: 'col-hidden',
-      'connector.ssl': 'col-ssl',
+      ssl: 'col-ssl col-need-row dividing-line-below',
       ...externalClass,
     }
   })
