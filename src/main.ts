458852dfad0e41e516dc00d153f3ffb183abf76e
Kinplemelon <wujiu529@gmail.com>
2023-01-16 18:21:06 +0800

    feat(global): add directive `v-safe-html`

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,17 @@ import 'highlight.js/styles/monokai-sublime.css'
 import ElementPlus from 'element-plus'
 import elementI18nZhCn from 'element-plus/es/locale/lang/zh-cn'
 import elementI18nEn from 'element-plus/es/locale/lang/en'
+import safeHTML from '@/common/safeHTML'
 
 import i18n from './i18n'
 import EMQSelect from '@/components/EmqSelect.vue'
 
 function globalComponents(app: Application) {
   app.component(EMQSelect.name, EMQSelect)
+}
+
+function bindDirective(app: Application) {
+  app.directive('safe-html', safeHTML)
 }
 
 const elementLang = store.state.lang === 'en' ? elementI18nEn : elementI18nZhCn
@@ -25,6 +30,7 @@ const app = createApp(App)
   })
   .use(i18n)
   .use(globalComponents)
+  .use(bindDirective)
   .mount('#app')
 
 export default app
