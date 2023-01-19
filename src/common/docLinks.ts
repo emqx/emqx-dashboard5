49115b7c30bb55ff04e54e62b19e6dc03220ecb3
Kinplemelon <wujiu529@gmail.com>
2023-01-19 15:50:21 +0800

    refactor(doc link): update doc link for enterprise version

--- src/common/docLinks.ts
+++ src/common/docLinks.ts
@@ -55,22 +55,22 @@ export default (lang: string): DocMap => {
     sqlGrammar: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-sql-syntax.html`,
     cloud: `https://${accountsLink}/signup?${QUERY_FOR_GO_CLOUD}`,
     // TODO: version
-    documentation: `https://www.emqx.io/docs/${lang}/v5.0/?${QUERY_FOR_HELP}`,
+    documentation: `https://docs.emqx.com/${lang}/enterprise/v5.0/?${QUERY_FOR_HELP}`,
     forum: lang === 'en' ? `https://www.emqx.io/forum/` : `https://askemq.com/`,
     gitHub: `https://github.com/emqx/emqx`,
-    ruleEvent: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-sql-events-and-fields.html`,
-    bridgeAsFrom: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-sql-events-and-fields.html#${
+    ruleEvent: `https://docs.emqx.com/${lang}/enterprise/v5.0/data-integration/rule-sql-events-and-fields.html`,
+    bridgeAsFrom: `https://docs.emqx.com/${lang}/enterprise/v5.0/data-integration/rule-sql-events-and-fields.html#${
       lang === 'zh' ? '数据桥接' : 'data-bridges'
     }`,
-    ruleEventMsgPub: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/rule-sql-events-and-fields.html#${
+    ruleEventMsgPub: `https://docs.emqx.com/${lang}/enterprise/v5.0/data-integration/rule-sql-events-and-fields.html#${
       lang === 'zh' ? 'mqtt-消息' : 'mqtt-message'
     }`,
     enterprise: `https://www.emqx.com/${lang}/products/emqx?${QUERY_FOR_GO_ENTERPRISE}`,
     blog: `https://www.emqx.com/${lang}/blog/category/emqx?${QUERY_FOR_HELP}`,
-    dashboard: `https://www.emqx.io/docs/${lang}/v5.0/dashboard/introduction.html?${QUERY_FOR_HELP}`,
-    emqxGettingStarted: `https://www.emqx.io/docs/${lang}/v5.0/getting-started.html?${QUERY_FOR_HELP}`,
-    accessControl: `https://www.emqx.io/docs/${lang}/v5.0/dashboard/authn.html?${QUERY_FOR_HELP}`,
-    dataIntegration: `https://www.emqx.io/docs/${lang}/v5.0/data-integration/introduction.html?${QUERY_FOR_HELP}`,
+    dashboard: `https://docs.emqx.com/${lang}/enterprise/v5.0/dashboard/introduction.html?${QUERY_FOR_HELP}`,
+    emqxGettingStarted: `https://docs.emqx.com/${lang}/enterprise/v5.0/getting-started.html?${QUERY_FOR_HELP}`,
+    accessControl: `https://docs.emqx.com/${lang}/enterprise/v5.0/dashboard/authn.html?${QUERY_FOR_HELP}`,
+    dataIntegration: `https://docs.emqx.com/${lang}/enterprise/v5.0/data-integration/introduction.html?${QUERY_FOR_HELP}`,
     mqttStudy: `https://www.emqx.com/${lang}/mqtt?${QUERY_FOR_HELP}`,
     mqttV5: `https://www.emqx.com/${lang}/mqtt/mqtt5?${QUERY_FOR_HELP}`,
     mqttClient:
@@ -83,7 +83,7 @@ export default (lang: string): DocMap => {
     linkedInHome: 'https://www.linkedin.com/company/emqtech',
     emqxEnterprise: `https://www.emqx.com/${lang}/products/emqx?${QUERY_FOR_HELP}`,
     cloudHome: `https://www.emqx.com/${lang}/cloud?${QUERY_FOR_HELP}`,
-    resetPassword: `https://www.emqx.io/docs/${lang}/v5.0/admin/cli.html#admins`,
+    resetPassword: `https://docs.emqx.com/${lang}/enterprise/v5.0/admin/cli.html#admins`,
     applyLicense: `https://www.emqx.com/${lang}/apply-licenses/emqx?product=emqx&utm_source=emqx-dashboard&utm_medium=referral&utm_campaign=dashboard-to-contact-us`,
   }
 }
