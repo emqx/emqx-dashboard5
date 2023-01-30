a924a3442ba5ff8dd4db061729debb66ebb8d193
Kinplemelon <wujiu529@gmail.com>
2023-01-30 14:00:12 +0800

    refactor(bridge webhook): add `pool_type`

--- src/i18n/RuleEngine.js
+++ src/i18n/RuleEngine.js
@@ -1009,6 +1009,10 @@ When disabled the messages are buffered in RAM only.`,
     en: 'Maximum batch waiting interval.',
     zh: '最大批量请求等待时间。',
   },
+  poolType: {
+    zh: '连接池类型',
+    en: 'Pool Type',
+  },
   connectionPoolSize: {
     zh: '连接池大小',
     en: 'Connection Pool Size',
