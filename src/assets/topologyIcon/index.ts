e95795fcc7191160f7d5a445faa924fa6ca9605c
Kinplemelon <wujiu529@gmail.com>
2023-03-13 10:51:22 +0800

    refactor(rule flows): add func for get node icon

--- src/assets/topologyIcon/index.ts
+++ src/assets/topologyIcon/index.ts
@@ -1,15 +1,13 @@
-import { BridgeType } from '@/types/enum'
-import bridgeWebhook from '@/assets/img/webhook.png'
-import bridgeMQTT from '@/assets/img/mqtt.png'
 import console from './console.png'
 import event from './event.png'
 import republish from './republish.png'
 import rule from './rule.png'
 import topic from './topic.png'
 
+const BRIDGE_PREFIX = 'bridge-'
+const bridgeReg = new RegExp(`^${BRIDGE_PREFIX}`)
+
 const iconMap: Record<string, SVGAElement> = {
-  [`bridge-${BridgeType.Webhook}`]: bridgeWebhook,
-  [`bridge-${BridgeType.MQTT}`]: bridgeMQTT,
   console,
   event,
   republish,
@@ -17,4 +15,18 @@ const iconMap: Record<string, SVGAElement> = {
   topic,
 }
 
-export default iconMap
+const getIcon = (target: string): SVGAElement | undefined => {
+  if (target in iconMap) {
+    return iconMap[target]
+  }
+  try {
+    if (bridgeReg.test(target)) {
+      return require(`@/assets/img/${target.replace(BRIDGE_PREFIX, '')}.png`)
+    }
+    return undefined
+  } catch (error) {
+    return undefined
+  }
+}
+
+export default getIcon
