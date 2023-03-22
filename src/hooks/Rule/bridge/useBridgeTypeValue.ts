86d366bbdd8543c37f8094b2ab5f90ca7a956d8c
Kinplemelon <wujiu529@gmail.com>
2023-03-22 15:27:59 +0800

    feat(bridge kafka): add kafka consumer

--- src/hooks/Rule/bridge/useBridgeTypeValue.ts
+++ src/hooks/Rule/bridge/useBridgeTypeValue.ts
@@ -83,7 +83,12 @@ export const useBridgeTypeOptions = (): {
   /**
    * Not a specific type, but a general type, such as influxdb v1 v2 are all influxdb
    */
-  const typesWithMultiSpecificType = [BridgeType.InfluxDB, BridgeType.Redis, BridgeType.MongoDB]
+  const typesWithMultiSpecificType = [
+    BridgeType.InfluxDB,
+    BridgeType.Redis,
+    BridgeType.MongoDB,
+    BridgeType.Kafka,
+  ]
   const getBridgeType = (typeStr: string): BridgeType => {
     if (!typeStr) {
       return typeStr as BridgeType
