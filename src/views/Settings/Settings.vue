08c84e0a0967a92bba657f2f1a93624bc93e4c99
Kinplemelon <wujiu529@gmail.com>
2023-05-29 17:31:36 +0800

    refactor(settings): simplify some code

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -155,9 +155,6 @@ const loadData = async () => {
     teleConfigs.value = res
   }
 }
-const reloading = () => {
-  loadData()
-}
 loadData()
 const handleSave = async () => {
   saveLoading.value = true
@@ -171,7 +168,7 @@ const handleSave = async () => {
     }
     await updateTeleStatus(data)
     ElMessage.success(t('Base.updateSuccess'))
-    reloading()
+    loadData()
   } catch (error) {
     // ignore error
   } finally {
