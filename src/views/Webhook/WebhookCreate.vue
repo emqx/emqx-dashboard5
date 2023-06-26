e914eaa0412975b96d440ee8b424f4f7194b71b8
Kinplemelon <wujiu529@gmail.com>
2023-06-26 17:13:49 +0800

    feat(webhook): webhook form (WIP)

---
+++ src/views/Webhook/WebhookCreate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-wrapper">
+    <el-card class="webhook-create-card">
+      <WebhookForm v-model="webhook" />
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import WebhookForm from './components/WebhookForm.vue'
+import useWebhookForm from '@/hooks/Webhook/useWebhookForm'
+
+const { createRawWebhookForm } = useWebhookForm()
+
+const webhook = ref(createRawWebhookForm())
+</script>
+
+<style lang="scss">
+.webhook-create-card {
+  padding: 12px 104px;
+}
+</style>
