c35b0d223b8ad1589630e38c1c6cf7c989967290
Kinplemelon <wujiu529@gmail.com>
2023-06-26 18:27:29 +0800

    feat(webhook): submit new webhook

--- src/types/webhook.d.ts
+++ src/types/webhook.d.ts
@@ -3,6 +3,7 @@ import { BasicRule, HTTPBridge } from './rule'
 export interface WebhookForm {
   bridge: HTTPBridge
   rule: BasicRule
+  name: string
 }
 
 export interface WebhookItem extends WebhookForm {
