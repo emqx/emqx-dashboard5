126df5d7c07528217da7f9e4ba7aa4e1f73627a5
Kinplemelon <wujiu529@gmail.com>
2023-06-27 11:23:51 +0800

    feat(webhook): webhook detail page

--- src/views/Webhook/WebhookCreate.vue
+++ src/views/Webhook/WebhookCreate.vue
@@ -3,39 +3,37 @@
     <el-card class="webhook-create-card">
       <WebhookFormCom v-model="webhook" />
       <div class="card-ft">
-        <el-button :loading="isSubmitting" type="primary" @click="submit">{{
-          tl('save')
-        }}</el-button>
+        <el-button :loading="isSubmitting" type="primary" @click="submit">
+          {{ tl('save') }}
+        </el-button>
       </div>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, Ref } from 'vue'
-import WebhookFormCom from './components/WebhookForm.vue'
+import { createBridge, createRules } from '@/api/ruleengine'
+import { checkNOmitFromObj, getBridgeKey } from '@/common/tools'
 import useWebhookForm from '@/hooks/Webhook/useWebhookForm'
 import useI18nTl from '@/hooks/useI18nTl'
-import { createRules, createBridge, testConnect } from '@/api/ruleengine'
 import { WebhookForm } from '@/types/webhook'
-import { WEBHOOK_PREFIX } from '@/common/constants'
 import { ElMessage } from 'element-plus'
+import { Ref, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { checkNOmitFromObj } from '@/common/tools'
-import { getBridgeKey } from '@/common/tools'
+import WebhookFormCom from './components/WebhookForm.vue'
 
 const { tl } = useI18nTl('Base')
 const router = useRouter()
 
-const { createRawWebhookForm } = useWebhookForm()
+const { createRawWebhookForm, getRuleIdByName, getBridgeNameByName } = useWebhookForm()
 
 const webhook: Ref<WebhookForm> = ref(createRawWebhookForm())
 const isSubmitting = ref(false)
 
 const setName = (data: WebhookForm) => {
   const { name } = data
-  data.bridge.name = `${WEBHOOK_PREFIX}${name}`
-  data.rule.id = `${WEBHOOK_PREFIX}${name}`
+  data.bridge.name = getBridgeNameByName(name)
+  data.rule.id = getRuleIdByName(name)
   data.rule.actions = [getBridgeKey(data.bridge)]
   return data
 }
@@ -45,8 +43,8 @@ const submit = async () => {
   try {
     isSubmitting.value = true
     // Because it is easier to report errors when creating bridge, put it in the front..
-    const bridge = await createBridge(data.bridge)
-    await createRules({ ...data.rule, actions: [bridge.id] })
+    await createBridge(data.bridge)
+    await createRules(data.rule)
     ElMessage.success(tl('createSuccess'))
     router.push({ name: 'webhook' })
   } catch (error) {
