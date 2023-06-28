1bdab01b1df424a55073aa103be01cd0cf05e23f
Kinplemelon <wujiu529@gmail.com>
2023-06-28 18:15:54 +0800

    feat(flow): handle click event of source node (WIP

---
+++ src/views/Flow/components/form/source/MessageForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-form
+    ref="FormCom"
+    label-width="240px"
+    class="message-form"
+    label-position="right"
+    hide-required-asterisk
+    :rules="rules"
+    :model="record"
+    :validate-on-rule-change="false"
+    @keyup.enter="saveConfig()"
+  >
+    <h3>Message Form</h3>
+  </el-form>
+</template>
+
+<script setup lang="ts">
+import { computed, defineEmits, defineProps, ref } from 'vue'
+
+const FormCom = ref()
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+const emit = defineEmits(['update:modelValue', 'save'])
+
+const record = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    emit('update:modelValue', val)
+  },
+})
+
+const rules = {}
+
+const saveConfig = () => {
+  emit('save', record.value)
+}
+</script>
+
+<style lang="scss"></style>
