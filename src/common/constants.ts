dd4ee6d04855e6909af4a49fbf75106be3db3a56
Kinplemelon <wujiu529@gmail.com>
2023-07-03 14:13:34 +0800

    feat(flow): filter form (WIP)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -47,6 +47,8 @@ export const DEFAULT_FROM = 't/#'
 
 export const DEFAULT_SELECT = '*'
 
+export const RULE_LOGICAL_OPERATORS = ['>', '<', '<=', '>=', '<>', '!=', '=', '=~']
+
 export const IP_REG =
   /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]):([0-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-5]{2}[0-3][0-5])$/
 
