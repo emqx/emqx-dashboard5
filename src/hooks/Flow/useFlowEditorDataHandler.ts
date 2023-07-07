2e6753f7915e2d5447584db3b43e61d3690776c1
Kinplemelon <wujiu529@gmail.com>
2023-07-07 18:04:08 +0800

    feat(flow): generate rule from nodes(WIP)

--- src/hooks/Flow/useFlowEditorDataHandler.ts
+++ src/hooks/Flow/useFlowEditorDataHandler.ts
@@ -1,14 +1,15 @@
 /**
  * Process the flow data from the flow editor and convert it into data that can be submitted.
  */
-import { BasicRule, BridgeItem } from '@/types/rule'
-import useRuleForm from '@/hooks/Rule/rule/useRuleForm'
-import { SourceType, ProcessingType, SinkType } from '@/hooks/Flow/useFlowEditor'
-import { getBridgeKey } from '@/common/tools'
-import { BridgeType, RuleOutput } from '@/types/enum'
 import { RULE_INPUT_BRIDGE_TYPE_PREFIX } from '@/common/constants'
-import { FilterForm, FilterItem } from './useNodeForm'
+import { getBridgeKey } from '@/common/tools'
+import { ProcessingType, SinkType, SourceType } from '@/hooks/Flow/useFlowEditor'
+import useRuleForm from '@/hooks/Rule/rule/useRuleForm'
 import { useRuleUtils } from '@/hooks/Rule/topology/useRule'
+import { BridgeType } from '@/types/enum'
+import { BasicRule, BridgeItem } from '@/types/rule'
+import { groupBy } from 'lodash'
+import { FilterForm, FilterItem } from './useNodeForm'
 
 const enum FlowNodeType {
   Input = 'input',
@@ -30,6 +31,8 @@ interface EdgeData {
   target: string
 }
 
+type NodesAfterGroup = Record<FlowNodeType, Array<NodeData>>
+
 interface FlowData {
   nodes: Array<NodeData>
   edges: Array<EdgeData>
@@ -38,13 +41,22 @@ interface FlowData {
 export default () => {
   const { createRawRuleForm } = useRuleForm()
 
-  const filterUsedNodesInFlowData = ({ nodes, edges }: FlowData) => {
+  const filterUsedNodesInFlowData = ({
+    nodes,
+    edges,
+  }: FlowData): {
+    nodes: NodesAfterGroup
+    edges: Array<EdgeData>
+  } => {
     // TODO:Check for incomplete data flows.
     const nodesWithEdge = edges.reduce(
       (arr, { source, target }) => arr.add(source).add(target),
       new Set(),
     )
-    const nodeRet = nodes.filter(({ id }) => nodesWithEdge.has(id))
+    const nodeRet = groupBy(
+      nodes.filter(({ id }) => nodesWithEdge.has(id)),
+      'type',
+    ) as NodesAfterGroup
     return { nodes: nodeRet, edges }
   }
 
@@ -83,30 +95,25 @@ export default () => {
       }
       const { specificType, formData } = node.data
       let data = ''
-      switch (specificType) {
-        case SinkType.Console:
-          data = RuleOutput.Console
-          break
-        case SinkType.RePub:
-          data = formData
-          break
-        // TODO:TODO:TODO:Various types of bridges.
-        // TODO:TODO:TODO:Various types of bridges.
-        // TODO:TODO:TODO:Various types of bridges.
-        default:
-          return ret
+      if (specificType === SinkType.Console || specificType === SinkType.RePub) {
+        data = formData
+        ret.push(data)
       }
-      ret.push(data)
+      // TODO:TODO:TODO:Various types of bridges.
+      // TODO:TODO:TODO:Various types of bridges.
+      // TODO:TODO:TODO:Various types of bridges.
       return ret
     }, [])
   }
 
-  const processFilterDataToSql = (filterData: FilterForm): string => {
+  const processFilterDataToSql = (filterData: FilterForm, level = 0): string => {
     if (Array.isArray(filterData.items)) {
       const clauses: Array<string> = filterData.items.map((item) =>
-        processFilterDataToSql(item as FilterForm),
+        processFilterDataToSql(item as FilterForm, level + 1),
       )
-      return '(' + clauses.join(` ${filterData.groupOperator.toUpperCase()} `) + ')'
+      return `${level > 0 ? '(' : ''}${clauses.join(` ${filterData.groupOperator} `)}${
+        level > 0 ? ')' : ''
+      }`
     }
     const { field, operator, valueForComparison } = filterData as unknown as FilterItem
     // TODO:Confirm how to determine the type of input data.
@@ -124,19 +131,44 @@ export default () => {
     return processFilterDataToSql(filterData)
   }
 
-  const { transFromDataArrToStr, transSQLFormDataToSQL } = useRuleUtils()
+  const getBridgesFromNodes = (flowName: string, nodes: Array<NodeData>): Array<BridgeItem> => {
+    const bridgeDataArr = nodes.reduce((arr: Array<BridgeItem>, { type, data }) => {
+      const isInputAndNotMessageOrEvent =
+        type === FlowNodeType.Input &&
+        data.specificType !== SourceType.Message &&
+        data.specificType !== SourceType.Event
+
+      const isOutputAndNotConsoleOrRePub =
+        type === FlowNodeType.Output &&
+        data.specificType !== SinkType.Console &&
+        data.specificType !== SinkType.RePub
+
+      if (isInputAndNotMessageOrEvent || isOutputAndNotConsoleOrRePub) {
+        arr.push({ ...data.formData, name: flowName })
+      }
+      return arr
+    }, [])
+    return bridgeDataArr
+  }
+
+  const { transSQLFormDataToSQL } = useRuleUtils()
   const getRuleNBridgesFromFlowData = (
     flowName: string,
     flowData: FlowData,
   ): { rule: BasicRule; bridges: Array<BridgeItem> } => {
-    const rule = { ...createRawRuleForm(), name: flowName }
-    const bridges: Array<BridgeItem> = []
+    const rule = { ...createRawRuleForm(), id: flowName }
     const { nodes, edges } = filterUsedNodesInFlowData(flowData)
-    const fromArr = getFromDataFromNodes(flowName, nodes)
-    const filterStr = getFilterStrFromNodes(nodes)
+    const {
+      [FlowNodeType.Input]: inputNodes = [],
+      [FlowNodeType.Default]: processingNodes = [],
+      [FlowNodeType.Output]: outputNodes = [],
+    } = nodes
+    const fromArr = getFromDataFromNodes(flowName, inputNodes)
+    const filterStr = getFilterStrFromNodes(processingNodes)
     // TODO:replace *
-    rule.sql = transSQLFormDataToSQL(transFromDataArrToStr(fromArr), ['*'], filterStr)
-    rule.actions = getActionDataFromNodes(flowName, nodes)
+    rule.sql = transSQLFormDataToSQL('*', fromArr, filterStr)
+    rule.actions = getActionDataFromNodes(flowName, outputNodes)
+    const bridges = getBridgesFromNodes(flowName, [...inputNodes, ...outputNodes])
     return { rule, bridges }
   }
 
