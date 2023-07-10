579433af562dc8b1828e9a76089723547bcb8783
Kinplemelon <wujiu529@gmail.com>
2023-07-10 14:20:37 +0800

    refactor(bridge): exact func to create MQTT bridge form

--- src/hooks/Rule/bridge/useBridgeFormCreator.ts
+++ src/hooks/Rule/bridge/useBridgeFormCreator.ts
@@ -1,9 +1,16 @@
-import { HTTPBridge } from '@/types/rule'
 import useResourceOpt from '@/hooks/Rule/bridge/useResourceOpt'
 import useSSL from '@/hooks/useSSL'
+import { BridgeDirection, BridgeType } from '@/types/enum'
+import {
+  HTTPBridge,
+  MQTTBridgeEgress,
+  MQTTBridgeIngress,
+  MQTTBridgeTransConfiguration,
+} from '@/types/rule'
 
 export default (): {
   createRawHTTPForm: () => HTTPBridge
+  createRawMQTTForm: (direction?: BridgeDirection) => any
 } => {
   const { createDefaultResourceOptsForm } = useResourceOpt()
   const { createSSLForm } = useSSL()
@@ -26,7 +33,53 @@ export default (): {
       ssl: createSSLForm(),
     } as HTTPBridge)
 
+  const createRawMQTTTransDefaultVal = (): MQTTBridgeTransConfiguration => ({
+    topic: '',
+    qos: 1,
+    payload: '${payload}',
+    retain: false,
+  })
+  const createMQTTIngressDefaultVal = (): MQTTBridgeIngress => ({
+    remote: { topic: '', qos: 1 },
+    local: createRawMQTTTransDefaultVal(),
+  })
+  const createMQTTEgressDefaultValue = (): MQTTBridgeEgress => ({
+    local: { topic: '' },
+    remote: createRawMQTTTransDefaultVal(),
+  })
+  const createRawMQTTForm = (direction: BridgeDirection = BridgeDirection.Both): any => {
+    const base = {
+      enable: true,
+      name: '',
+      server: '',
+      proto_ver: 'v4',
+      username: '',
+      password: '',
+      ssl: createSSLForm(),
+      keepalive: '300s',
+      retry_interval: '15s',
+      clean_start: true,
+      bridge_mode: false,
+      type: BridgeType.MQTT,
+      resource_opts: createDefaultResourceOptsForm({ inflight: true }),
+    }
+
+    switch (direction) {
+      case BridgeDirection.Ingress:
+        return { ...base, ingress: createMQTTIngressDefaultVal() }
+      case BridgeDirection.Egress:
+        return { ...base, egress: createMQTTEgressDefaultValue() }
+      default:
+        return {
+          ...base,
+          ingress: createMQTTIngressDefaultVal(),
+          egress: createMQTTEgressDefaultValue(),
+        }
+    }
+  }
+
   return {
     createRawHTTPForm,
+    createRawMQTTForm,
   }
 }
