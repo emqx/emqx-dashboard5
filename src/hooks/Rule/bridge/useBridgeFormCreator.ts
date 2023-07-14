aa5627eef737f6471c3da3ea3124cda3c1c0569c
Kinplemelon <wujiu529@gmail.com>
2023-07-14 13:55:36 +0800

    fix(bridge): change http pool type default value

--- src/hooks/Rule/bridge/useBridgeFormCreator.ts
+++ src/hooks/Rule/bridge/useBridgeFormCreator.ts
@@ -22,7 +22,7 @@ export default (): {
       url: 'http://',
       headers: { 'content-type': 'application/json' },
       body: '',
-      pool_type: 'random',
+      pool_type: 'hash',
       pool_size: 8,
       enable_pipelining: 100,
       connect_timeout: '15s',
