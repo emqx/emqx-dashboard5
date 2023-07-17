43c42b0fca9293f35e6bb435d5eefa62b2615ea6
Kinplemelon <wujiu529@gmail.com>
2023-07-17 11:30:07 +0800

    feat(flow): add interaction for change name and desc

--- src/i18n/Flow.js
+++ src/i18n/Flow.js
@@ -7,4 +7,12 @@ export default {
     zh: '列表',
     en: 'List',
   },
+  description: {
+    zh: '描述',
+    en: 'Description',
+  },
+  basicInfo: {
+    zh: '基本信息',
+    en: 'Basic Info',
+  },
 }
