3ee952b2777cf1e817c24ed405f67ccc57f79454
Kinplemelon <wujiu529@gmail.com>
2023-07-21 11:40:03 +0800

    feat(flow): validate flow data before submit

--- src/i18n/Flow.js
+++ src/i18n/Flow.js
@@ -59,4 +59,20 @@ export default {
     zh: '是否取消{type}？你所做的修改将不被保存',
     en: 'Are you sure you want to cancel {type}? Your changes will not be saved.',
   },
+  flowEmptyError: {
+    zh: '无法保存，Flow 中需要有至少一个 Sink 节点和一个 Source 节点',
+    en: 'Unable to save, there must be at least one Sink node and one Source node in the Flow',
+  },
+  flowIntegrityError: {
+    zh: '无法保存，Flow 中需要有至少一个 {missing} 节点',
+    en: 'Unable to save, at least one {missing} node is required in the Flow',
+  },
+  isolatedNodeError: {
+    zh: 'Flow 中有未连接的节点，请先连接或将其删除|Flow 中有未连接的节点，请先连接或将其删除',
+    en: 'There are unconnected node in the flow. Please connect or delete them.|There are unconnected nodes in the flow. Please connect or delete them.',
+  },
+  multipleFlowError: {
+    zh: '每次仅允许创建一个 Flow',
+    en: 'Only one flow can be created at a time',
+  },
 }
