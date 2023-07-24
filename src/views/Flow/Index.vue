6bfbef2f91fd984fde8abb43f7db1937743aa523
Kinplemelon <wujiu529@gmail.com>
2023-07-24 17:35:41 +0800

    feat(flow): add list page

--- src/views/Flow/Index.vue
+++ src/views/Flow/Index.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="flow" v-loading="isLoading">
-    <template v-if="flowData.length">
+    <template v-if="showData">
       <div class="flow-view-hd space-between vertical-align-center">
         <el-radio-group v-model="showBy">
           <el-radio-button :label="ShowByOpt.Flow">Flow</el-radio-button>
-          <el-radio-button :label="ShowByOpt.List" disabled>{{ tl('list') }}</el-radio-button>
+          <el-radio-button :label="ShowByOpt.List">{{ tl('list') }}</el-radio-button>
         </el-radio-group>
         <el-button @click="goCreate" type="primary">{{ tl('createFlow') }}</el-button>
       </div>
-      <VueFlow class="editor" ref="FlowerInstance" v-model="flowData" @node-click="handleClickNode">
-        <template #node-custom_input="data"><FlowNode :data="data" /></template>
-        <template #node-custom_default="data"><FlowNode :data="data" /></template>
-        <template #node-custom_output="data"><FlowNode :data="data" /></template>
-      </VueFlow>
+      <FlowView v-if="showBy === ShowByOpt.Flow" @loaded="handleLoaded" />
+      <FlowList v-if="showBy === ShowByOpt.List" />
     </template>
     <div v-else class="flow-placeholder-container">
       <img
@@ -27,14 +24,12 @@
 </template>
 
 <script setup lang="ts">
-import useFlowView from '@/hooks/Flow/useFlowView'
 import useI18nTl from '@/hooks/useI18nTl'
-import { VueFlow, useVueFlow } from '@vue-flow/core'
-import { onMounted, ref } from 'vue'
+import { useVueFlow } from '@vue-flow/core'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import FlowNode from './components/FlowNode.vue'
-
-const FlowerInstance = ref()
+import FlowView from './components/FlowView.vue'
+import FlowList from './components/FlowList.vue'
 
 const router = useRouter()
 const { tl } = useI18nTl('Flow')
@@ -45,7 +40,13 @@ const enum ShowByOpt {
 }
 const showBy = ref(ShowByOpt.Flow)
 
-const { isLoading, flowData, getFlowData } = useFlowView()
+const isLoading = ref(true)
+const showData = ref(true)
+
+const handleLoaded = (dataLength: number) => {
+  isLoading.value = false
+  showData.value = dataLength > 0
+}
 
 useVueFlow({
   deleteKeyCode: null,
@@ -54,17 +55,6 @@ useVueFlow({
 })
 
 const goCreate = () => router.push({ name: 'flow-create' })
-
-const handleClickNode = () => {
-  /*  */
-}
-
-onMounted(async () => {
-  await getFlowData()
-  if (flowData.value.length > 0) {
-    FlowerInstance.value?.fitView()
-  }
-})
 </script>
 
 <style lang="scss">
@@ -79,7 +69,7 @@ onMounted(async () => {
     padding: 0 24px;
     border-bottom: 1px solid var(--color-border-primary);
   }
-  .vue-flow {
+  .flow-view {
     height: calc(100% - #{$hd-height});
   }
   .flow-placeholder-container {
