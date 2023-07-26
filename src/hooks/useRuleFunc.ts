3285f46c5d6cac82260abc7105c8382633815757
Kinplemelon <wujiu529@gmail.com>
2023-07-26 18:57:56 +0800

    feat(flow): handle select func

--- src/hooks/useRuleFunc.ts
+++ src/hooks/useRuleFunc.ts
@@ -1,13 +1,59 @@
-import useI18nTl from './useI18nTl'
 import RuleFunc from '@/hooks/Rule/RuleFunc.json'
 
+export const enum ArgumentType {
+  Number = 'number',
+  Any = 'any',
+  Float = 'float',
+  Integer = 'integer',
+  String = 'string',
+  Enum = 'enum',
+  Object = 'Object',
+  Array = 'array',
+  Binary = 'binary',
+}
+
+export interface ArgItem {
+  name: string
+  type: ArgumentType
+  required: boolean
+  optionalValues?: Array<string>
+  /**
+   * for type number
+   */
+  range?: [null | number, null | number]
+  default?: string | number
+}
+
+interface FuncItem {
+  name: string
+  args: Array<ArgItem>
+}
+
+interface GroupFuncData {
+  groupLabel: string
+  list: Array<FuncItem>
+}
+
+type FuncData = Array<GroupFuncData>
+
 export default () => {
-  const { t, tl } = useI18nTl('Function')
-  const funcOptList = RuleFunc.map(({ groupLabel, list }) => ({
+  const funcOptList: FuncData = RuleFunc.map(({ groupLabel, list }) => ({
     groupLabel,
-    list: list.filter((item) => item.args.length),
+    list: list.filter((item) => item.args.length) as Array<FuncItem>,
   }))
+
+  const getFuncItemByName = (name: string): FuncItem | null => {
+    for (const { list } of funcOptList) {
+      for (const item of list) {
+        if (item.name === name) {
+          return item
+        }
+      }
+    }
+    return null
+  }
   return {
     funcOptList,
+    getFuncItemByName,
   }
 }
--- src/i18n/Function.js
+++ src/i18n/Function.js
@@ -1,4 +1,47 @@
 /* Built-in SQL Functions */
 export default {
-  
+  mathFunc: {
+    zh: '数学函数',
+    en: 'Mathematical Functions',
+  },
+  dataTypeJudgmentFunc: {
+    zh: '数据类型判断函数',
+    en: 'Data Type Judgment Functions',
+  },
+  dataTypeConversionFunc: {
+    zh: '数据类型转换函数',
+    en: 'Data Type Conversion Functions',
+  },
+  stringFunc: {
+    zh: '字符串函数',
+    en: 'String Functions',
+  },
+  mapFunc: {
+    zh: 'Map 函数',
+    en: 'Map Functions',
+  },
+  arrFunc: {
+    zh: '数组函数',
+    en: 'Array Functions',
+  },
+  hashFunc: {
+    zh: '哈希函数',
+    en: 'Hash Functions',
+  },
+  compressesAndUncompressesFunc: {
+    zh: '压缩解压缩函数',
+    en: 'Compression and Decompression Functions',
+  },
+  bitFunc: {
+    zh: '比特操作函数',
+    en: 'Bit Functions',
+  },
+  decodingAndEncodingFunc: {
+    zh: '编解码函数',
+    en: 'Decoding and Encoding Functions',
+  },
+  timeAndDateFunc: {
+    zh: '时间与日期函数',
+    en: 'Time and Date Functions',
+  },
 }
