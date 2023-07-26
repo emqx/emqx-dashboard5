27ca8f60227820b0b55f5210d8ab73f88554bef8
YuShifan <894402575bt@gmail.com>
2023-07-26 10:03:34 +0800

    refactor(login): change the login page bg color

--- src/views/Base/Login.vue
+++ src/views/Base/Login.vue
@@ -293,7 +293,7 @@ const submitNewPwd = async () => {
   .el-row {
     height: 100%;
     .el-col.intro {
-      background-color: #1f252f;
+      background-color: var(--color-bg);
       position: relative;
       .logo {
         padding: 32px;
@@ -330,6 +330,7 @@ const submitNewPwd = async () => {
       }
     }
     .el-col.form {
+      background-color: var(--color-bg-content);
       position: relative;
       display: flex;
       justify-content: center;
