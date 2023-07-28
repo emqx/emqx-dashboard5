4cf4cfa9016d426e205c69e182f9e8868a27af78
Kinplemelon <wujiu529@gmail.com>
2023-07-28 15:13:16 +0800

    feat(flow): add labels to function params

--- src/i18n/Function.js
+++ src/i18n/Function.js
@@ -44,4 +44,149 @@ export default {
     zh: '时间与日期函数',
     en: 'Time and Date Functions',
   },
+  /* PARAMS */
+  Value: {
+    zh: '值',
+    en: 'Value',
+  },
+  Base: {
+    zh: '基数',
+    en: 'Base',
+  },
+  Exponent: {
+    zh: '指数',
+    en: 'Exponent',
+  },
+  Numer: {
+    zh: '分子',
+    en: 'Numer',
+  },
+  Denom: {
+    zh: '分母',
+    en: 'Denom',
+  },
+  Precision: {
+    zh: '精度',
+    en: 'Precision',
+  },
+  String: {
+    zh: '字符串',
+    en: 'String',
+  },
+  StartPosition: {
+    zh: '起始位置',
+    en: 'Start Position',
+  },
+  Separator: {
+    zh: '',
+    en: 'Separator',
+  },
+  Direction: {
+    zh: '方向',
+    en: 'Direction',
+  },
+  String1: {
+    zh: '左字符串',
+    en: 'String1',
+  },
+  String2: {
+    zh: '右字符串',
+    en: 'String2',
+  },
+  PreserveCRLF: {
+    zh: '是否保留换行符',
+    en: 'Preserve CRLF',
+  },
+  TotalLength: {
+    zh: '字符总长度',
+    en: 'Total Length',
+  },
+  PaddingDirection: {
+    zh: '补足方向',
+    en: 'Padding Direction',
+  },
+  PaddingString: {
+    zh: '补足字符',
+    en: 'Padding String',
+  },
+  OldSubStr: {
+    zh: '需要替换的字符串',
+    en: 'Old Sub Str',
+  },
+  Replacement: {
+    zh: '替换值',
+    en: 'Replacement',
+  },
+  ReplaceCount: {
+    zh: '替换位置',
+    en: 'Replace Count',
+  },
+  Pattern: {
+    zh: '正则表达式',
+    en: 'Pattern',
+  },
+  Character: {
+    zh: '字符',
+    en: 'Character',
+  },
+  SubstringToBeFound: {
+    zh: '要查找的子串',
+    en: 'Substring To Be Found',
+  },
+  DirectionToFind: {
+    zh: '查找方向',
+    en: 'Direction To Find',
+  },
+  Key: {
+    zh: '键',
+    en: 'Key',
+  },
+  Map: {
+    zh: 'Map',
+    en: 'Map',
+  },
+  DefaultValue: {
+    zh: '默认值',
+    en: 'Default Value',
+  },
+  Index: {
+    zh: '索引',
+    en: 'Index',
+  },
+  Array: {
+    zh: '数组',
+    en: 'Array',
+  },
+  StartIndex: {
+    zh: '起始索引',
+    en: 'Start Index',
+  },
+  Length: {
+    zh: '截取长度',
+    en: 'Length',
+  },
+  UnixTimestamp: {
+    zh: 'Unix 时间戳',
+    en: 'Unix Timestamp',
+  },
+  TimeUnit: {
+    zh: '时间单位',
+    en: 'Time Unit',
+  },
+  RFC3339String: {
+    zh: 'RFC 3339 时间字符串',
+    en: 'RFC3339 Time String',
+  },
+  TimeOffset: {
+    zh: '时区偏移',
+    en: 'Time Offset',
+  },
+  DateFormat: {
+    zh: '日期格式',
+    en: 'Date Format',
+  },
+  Timestamp: {
+    zh: '时间戳',
+    en: 'Timestamp',
+  },
 }
