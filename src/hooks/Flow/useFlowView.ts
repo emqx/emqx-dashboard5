c6e19e5b95cc0ab58faf8fabf33971518dbeea98
Kinplemelon <wujiu529@gmail.com>
2023-08-01 18:15:57 +0800

    refactor(flow): extract some utils for flow

--- src/hooks/Flow/useFlowView.ts
+++ src/hooks/Flow/useFlowView.ts
@@ -1,31 +1,13 @@
 import { getBridgeList, getRules } from '@/api/ruleengine'
-import {
-  BRIDGE_TYPES_WITH_TWO_DIRECTIONS,
-  DEFAULT_SELECT,
-  RULE_INPUT_BRIDGE_TYPE_PREFIX,
-  RULE_INPUT_EVENT_PREFIX,
-} from '@/common/constants'
-import { getAllListData, getKeyPartsFromSQL, trimSpacesAndLFs, splitOnComma } from '@/common/tools'
-import { useBridgeTypeOptions } from '@/hooks/Rule/bridge/useBridgeTypeValue'
+import { getAllListData } from '@/common/tools'
+import useFlowUtils from '@/hooks/Flow/useFlowUtils'
 import useRuleEvents from '@/hooks/Rule/rule/useRuleEvents'
-import { useRuleUtils } from '@/hooks/Rule/topology/useRule'
-import useRuleFunc, { ArgItem } from '@/hooks/useRuleFunc'
-import { BridgeDirection, BridgeType } from '@/types/enum'
-import { BridgeItem, OutputItem, OutputItemObj, RuleItem } from '@/types/rule'
+import { BridgeItem, RuleItem } from '@/types/rule'
 import { Edge, Node } from '@vue-flow/core'
-import { escapeRegExp, isString, unionBy } from 'lodash'
+import { unionBy } from 'lodash'
 import { Ref, ref } from 'vue'
 import useWebhookUtils from '../Webhook/useWebhookUtils'
-import useFlowNode, {
-  FlowData,
-  NodeType,
-  ProcessingType,
-  SinkType,
-  SourceType,
-  getSpecificTypeWithDirection,
-} from './useFlowNode'
-import { createEventForm, createFunctionItem, createMessageForm } from './useNodeForm'
-import useParseWhere from './useParseWhere'
+import useFlowNode, { FlowData, FlowNodeType, NodeType, ProcessingType } from './useFlowNode'
 
 /**
  * ID rule of each node
@@ -37,16 +19,6 @@ import useParseWhere from './useParseWhere'
  * - filter - `filter-{ruleID}`
  * - function - `function-{ruleID}`
  */
-
-/**
- * Sort by column
- */
-type GroupedNode = {
-  [NodeType.Source]: Array<Node>
-  [ProcessingType.Filter]: Array<Node>
-  [ProcessingType.Function]: Array<Node>
-  [NodeType.Sink]: Array<Node>
-}
 
 export default (): {
   isLoading: Ref<boolean>
@@ -70,10 +42,10 @@ export default (): {
   const isLoading = ref(false)
   const flowData: Ref<FlowData> = ref([])
 
-  const { transFromStrToFromArr } = useRuleUtils()
-  const { getTypeCommonData, getTypeLabel, getNodeInfo, isBridgerNode } = useFlowNode()
+  const { isBridgerNode } = useFlowNode()
 
   const { judgeIsWebhookBridge, judgeIsWebhookRule } = useWebhookUtils()
+
   const getRuleData = async () => {
     try {
       const data = await getAllListData(getRules)
@@ -94,307 +66,8 @@ export default (): {
     }
   }
 
-  const { getBridgeType } = useBridgeTypeOptions()
-  const getBridgeTypeFromId = (id: string): BridgeType => {
-    const type = id.slice(0, id.indexOf(':'))
-    return getBridgeType(type)
-  }
-
-  const getBridgeNameFromId = (id: string): string => id.slice(id.indexOf(':'))
-
-  const detectInputType = (from: string): string => {
-    if (from.indexOf(RULE_INPUT_EVENT_PREFIX) > -1) {
-      return SourceType.Event
-    }
-    // now has mqtt & http
-    const reg = new RegExp(`^${escapeRegExp(RULE_INPUT_BRIDGE_TYPE_PREFIX)}`)
-    if (reg.test(from)) {
-      return getBridgeTypeFromId(from.replace(RULE_INPUT_BRIDGE_TYPE_PREFIX, ''))
-    }
-    return SourceType.Message
-  }
-
-  const isTwoDirectionBridge = (bridgeType: string): boolean =>
-    BRIDGE_TYPES_WITH_TWO_DIRECTIONS.includes(bridgeType as BridgeType)
-
-  const getFormDataByType = (type: string, value: string) => {
-    if (type === SourceType.Event) {
-      return createEventForm(value)
-    } else if (type === SourceType.Message) {
-      return createMessageForm(value)
-    }
-    return { name: getBridgeNameFromId(value) }
-  }
-
-  /**
-   * generate input node
-   * - Message
-   * - Event
-   * - Bridge
-   */
-  const generateNodesBaseFromData = (fromArr: Array<string>) => {
-    return fromArr.reduce((arr: Array<Node>, fromItem): Array<Node> => {
-      const type = detectInputType(fromItem)
-      let specificType = type
-      if (
-        type !== SourceType.Event &&
-        type !== SourceType.Message &&
-        isTwoDirectionBridge(specificType)
-      ) {
-        specificType = getSpecificTypeWithDirection(
-          specificType as BridgeType,
-          BridgeDirection.Ingress,
-        )
-      }
-      const formData = getFormDataByType(type, fromItem)
-      const id =
-        type === SourceType.Event || type === SourceType.Message
-          ? `${type}-${fromItem}`
-          : `${specificType}-${fromItem.replace(RULE_INPUT_BRIDGE_TYPE_PREFIX, '')}`
-
-      const node = {
-        id,
-        ...getTypeCommonData(NodeType.Source),
-        label: getTypeLabel(specificType),
-        position: { x: 0, y: 0 },
-        data: { specificType, formData, desc: '' },
-      }
-      node.data.desc = getNodeInfo(node)
-      arr.push(node)
-      return arr
-    }, [])
-  }
-
-  const { generateFilterForm } = useParseWhere()
-  /**
-   * generate filter node
-   */
-  const generateNodeBaseWhereData = (whereStr: string, ruleId: string): Node => {
-    const node = {
-      id: `${ProcessingType.Filter}-${ruleId}`,
-      ...getTypeCommonData(NodeType.Processing),
-      label: getTypeLabel(ProcessingType.Filter),
-      position: { x: 0, y: 0 },
-      data: {
-        specificType: ProcessingType.Filter,
-        formData: generateFilterForm(whereStr),
-        desc: '',
-      },
-    }
-    node.data.desc = getNodeInfo(node)
-    return node
-  }
-
-  const { getFuncGroupByName, getFuncItemByName, getArgIndex } = useRuleFunc()
-  const countArgsWhenLengthNotMatch = (
-    functionParamTemplate: Array<ArgItem>,
-    actualParams: Array<string | number>,
-  ) => {
-    let startIndex = -1
-    return functionParamTemplate.map((item, index) => {
-      if (item.required && startIndex < 0) {
-        startIndex = index
-      }
-      const argIndex = index - startIndex
-      return startIndex > -1 && actualParams[argIndex] !== undefined ? actualParams[argIndex] : ''
-    })
-  }
-
-  const getFuncDataFromExpression = (
-    expression: string,
-  ): { field: string | number; func: { name: string; args: Array<string | number> } } => {
-    const funcName = expression.slice(0, expression.indexOf('('))
-    const funcGroup = getFuncGroupByName(funcName)
-    const funcItem = getFuncItemByName(funcName)
-    if (!funcGroup || !funcItem) {
-      throw new Error(`can not find function ${funcName}`)
-    }
-    const argIndex = getArgIndex(funcItem, funcGroup)
-    const funcArgs = expression
-      .slice(expression.indexOf('(') + 1, expression.lastIndexOf(')'))
-      .split(',')
-      .map((item) => item.trim())
-    let args: Array<string | number> = []
-    if (funcArgs.length !== funcItem.args.length) {
-      args = countArgsWhenLengthNotMatch(funcItem.args, funcArgs)
-    } else {
-      args = funcArgs
-    }
-    return { func: { name: funcName, args }, field: args[argIndex] }
-  }
-
-  const funcExpressionReg = /^(\w|_)+\(.|\n+\)/
-  const aliasPartReg = /\sas\s(\S+)/
-  const aliasReg = new RegExp(`.+${aliasPartReg.source}`)
-  const generateFunctionFormFromExpression = (expression: string) => {
-    const form = createFunctionItem()
-    const withAlias = aliasReg.test(expression)
-    if (withAlias) {
-      const [, alias = ''] = expression.match(aliasReg) || []
-      form.alias = alias
-    }
-
-    const selection = expression.replace(aliasPartReg, '')
-
-    if (funcExpressionReg.test(selection)) {
-      return { ...form, ...getFuncDataFromExpression(selection) }
-    }
-    return { ...form, field: selection }
-  }
-
-  const generateNodeBaseFieldsExpressions = (fieldsExpressions: string, ruleId: string) => {
-    if (trimSpacesAndLFs(fieldsExpressions) === DEFAULT_SELECT) {
-      return
-    }
-    const expressionArr = splitOnComma(fieldsExpressions).map((item) => trimSpacesAndLFs(item))
-    const formData = expressionArr.map((item) => generateFunctionFormFromExpression(item))
-    const node = {
-      id: `${ProcessingType.Function}-${ruleId}`,
-      ...getTypeCommonData(NodeType.Processing),
-      label: getTypeLabel(ProcessingType.Function),
-      position: { x: 0, y: 0 },
-      data: {
-        specificType: ProcessingType.Function,
-        formData,
-        desc: '',
-      },
-    }
-    node.data.desc = getNodeInfo(node)
-    return node
-  }
-
-  const detectOutputType = (action: OutputItem): string => {
-    if (isString(action)) {
-      return getBridgeTypeFromId(action)
-    } else {
-      const { function: func } = action
-      if (func === SinkType.Console) {
-        return SinkType.Console
-      } else if (action.args?.topic) {
-        return SinkType.RePub
-      }
-    }
-    return ''
-  }
-
-  const generateNodesBaseActions = (actions: Array<OutputItem>): Array<Node> => {
-    return actions.reduce((arr: Array<Node>, item): Array<Node> => {
-      const type = detectOutputType(item)
-      if (!type) {
-        return arr
-      }
-
-      let specificType = type
-      if (
-        type !== SinkType.Console &&
-        type !== SinkType.RePub &&
-        isTwoDirectionBridge(specificType)
-      ) {
-        specificType = getSpecificTypeWithDirection(
-          specificType as BridgeType,
-          BridgeDirection.Egress,
-        )
-      }
-
-      let id = ''
-      let formData = {}
-
-      if (type === SinkType.Console) {
-        id = SinkType.Console
-      } else if (type === SinkType.RePub) {
-        id = `${SinkType.RePub}-${(item as OutputItemObj).args?.topic}`
-        formData = item
-      } else {
-        id = `${specificType}-${item}`
-        formData = { name: getBridgeNameFromId(item as string) }
-      }
-
-      const node: Node = {
-        id,
-        ...getTypeCommonData(NodeType.Sink),
-        label: getTypeLabel(specificType),
-        position: { x: 0, y: 0 },
-        data: { specificType, formData, desc: '' },
-      }
-      node.data.desc = getNodeInfo(node)
-
-      arr.push(node)
-      return arr
-    }, [])
-  }
-
-  const generateEdgesFromNodes = (nodes: GroupedNode): Array<Edge> => {
-    const keys: Array<keyof GroupedNode> = [
-      NodeType.Source,
-      ProcessingType.Filter,
-      ProcessingType.Function,
-      NodeType.Sink,
-    ]
-    const result: Edge[] = []
-
-    for (let i = 0; i < keys.length - 1; i++) {
-      const currentKey: keyof GroupedNode = keys[i]
-
-      let nextKeyIndex = i + 1
-      let nextKey: keyof GroupedNode = keys[nextKeyIndex]
-
-      if (nodes[currentKey]?.length === 0) continue
-
-      while (nodes[nextKey]?.length === 0 && i < keys.length - 2) {
-        nextKeyIndex += 1
-        nextKey = keys[nextKeyIndex]
-      }
-      if (nodes[currentKey] && nodes[nextKey]) {
-        nodes[currentKey].forEach((cur) => {
-          nodes[nextKey].forEach((nex) => {
-            result.push({
-              id: `${cur.id}-${nex.id}`,
-              source: cur.id,
-              target: nex.id,
-            })
-          })
-        })
-      }
-    }
-    return result
-  }
-
-  /**
-   * Generate message, event, filter, and function nodes based on the SQL of the rule.
-   * Generate bridge, console, and republish nodes based on the actions.
-   * And the corresponding edges.
-   */
-  const generateFlowDataFromRuleItem = ({
-    sql,
-    actions,
-    id,
-    from,
-  }: RuleItem): { nodes: GroupedNode; edges: Edge[] } => {
-    const nodes: GroupedNode = {
-      [NodeType.Source]: [],
-      [ProcessingType.Filter]: [],
-      [ProcessingType.Function]: [],
-      [NodeType.Sink]: [],
-    }
-    const { fieldStr, whereStr } = getKeyPartsFromSQL(sql)
-    if (from && from.length > 0) {
-      nodes[NodeType.Source] = generateNodesBaseFromData(from)
-    }
-    if (whereStr !== undefined) {
-      nodes[ProcessingType.Filter].push(generateNodeBaseWhereData(whereStr, id))
-    }
-    if (fieldStr !== undefined) {
-      const node = generateNodeBaseFieldsExpressions(fieldStr, id)
-      if (node) {
-        nodes[ProcessingType.Function].push(node)
-      }
-    }
-    if (actions.length > 0) {
-      nodes[NodeType.Sink] = generateNodesBaseActions(actions)
-    }
-    const edges: Array<Edge> = generateEdgesFromNodes(nodes)
-    return { nodes, edges }
-  }
+  const { generateNodeFromBridgeData, generateFlowDataFromRuleItem, countNodesPosition } =
+    useFlowUtils()
 
   const addRuleDataToNodes = (nodes: Array<Node>, ruleId: string) =>
     nodes.map((node) => {
@@ -423,59 +96,21 @@ export default (): {
   const generateFlowDataFromRuleData = (ruleArr: Array<RuleItem>) => {
     ruleArr.forEach((rule) => {
       const { nodes, edges } = generateFlowDataFromRuleItem(rule)
+      Object.entries(nodes).forEach(([, value]) => addRuleDataToNodes(value, rule.id))
 
-      sourceNodes = addNodesToNodeArr(
-        addRuleDataToNodes(nodes[NodeType.Source], rule.id),
-        sourceNodes,
-      )
+      sourceNodes = addNodesToNodeArr(nodes[NodeType.Source], sourceNodes)
       filterNodes.push(...nodes[ProcessingType.Filter])
       functionNodes.push(...nodes[ProcessingType.Function])
-      sinkNodes = addNodesToNodeArr(addRuleDataToNodes(nodes[NodeType.Sink], rule.id), sinkNodes)
+      sinkNodes = addNodesToNodeArr(nodes[NodeType.Sink], sinkNodes)
 
       edgeArr.push(...edges)
     })
   }
   const generateNodesFromBridgeData = (bridgeArr: Array<BridgeItem>) => {
     bridgeArr.forEach((bridge) => {
-      const { type } = bridge
-      let specificType = type
-      let direction = BridgeDirection.Egress
-
-      if (isTwoDirectionBridge(type)) {
-        if (type === BridgeType.MQTT && 'ingress' in bridge) {
-          direction = BridgeDirection.Ingress
-        }
-        specificType = getSpecificTypeWithDirection(type, direction)
-      }
-      // TODO: for kafka,gcp...detect direction
-      const nodeType = direction === BridgeDirection.Ingress ? NodeType.Source : NodeType.Sink
-      const targetNodes = direction === BridgeDirection.Ingress ? sourceNodes : sinkNodes
-      const node: Node = {
-        id: `${specificType}-${bridge.id}`,
-        position: { x: 0, y: 0 },
-        label: getTypeLabel(specificType),
-        ...getTypeCommonData(nodeType),
-        data: { specificType, formData: bridge, desc: '' },
-      }
-      node.data.desc = getNodeInfo(node)
+      const node = generateNodeFromBridgeData(bridge)
+      const targetNodes = node.type === FlowNodeType.Input ? sourceNodes : sinkNodes
       targetNodes.push(node)
-    })
-  }
-
-  const nodeWidth = 200
-  const nodeHeight = 60
-  const nodeColumnSpacing = 100
-  const nodeRowSpacing = 30
-  const setPositionToColumnNodes = (
-    columnNodes: Array<Node>,
-    columnIndex: number,
-    totalHeight: number,
-  ) => {
-    const columnTotalHeight = columnNodes.length * (nodeHeight + nodeRowSpacing) - nodeRowSpacing
-    const x = (nodeWidth + nodeColumnSpacing) * columnIndex
-    const startY = (totalHeight - columnTotalHeight) / 2
-    columnNodes.forEach((node, index) => {
-      node.position = { x, y: startY + index * (nodeRowSpacing + nodeHeight) }
     })
   }
 
@@ -512,14 +147,14 @@ export default (): {
     ;[sourceNodes, sinkNodes] = nodeArrays
   }
 
-  const countNodesPosition = () => {
-    const totalHeight =
-      Math.max(sourceNodes.length, filterNodes.length, functionNodes.length, sinkNodes.length) *
-        (nodeHeight + nodeRowSpacing) -
-      nodeRowSpacing
-    ;[sourceNodes, filterNodes, functionNodes, sinkNodes].forEach((arr, index) => {
-      setPositionToColumnNodes(arr, index, totalHeight)
-    })
+  const setPositionToNodes = () => {
+    const nodes = {
+      [NodeType.Source]: sourceNodes,
+      [ProcessingType.Filter]: filterNodes,
+      [ProcessingType.Function]: functionNodes,
+      [NodeType.Sink]: sinkNodes,
+    }
+    countNodesPosition(nodes)
   }
 
   const joinToFlowData = () => {
@@ -543,7 +178,7 @@ export default (): {
     removeDuplicatedNodes()
     removeIsolatedBridge()
     setClassToRemovedBridges()
-    countNodesPosition()
+    setPositionToNodes()
     joinToFlowData()
   }
 
