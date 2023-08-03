e92c027c4abbb6cef62e8f3e46e90b3de027c97d
Kinplemelon <wujiu529@gmail.com>
2023-08-03 09:43:48 +0800

    fix(overview): remove test data

--- src/api/common.ts
+++ src/api/common.ts
@@ -65,48 +65,12 @@ export function setPrometheus(body: Prometheus): Promise<Prometheus> {
   return http.put('/prometheus', body)
 }
 
-const nodesNum = 6
-const totalCore = 6
-
-let arr: Array<boolean> = []
-
-const createArr = () => {
-  let coreCount = 0
-  arr = new Array(nodesNum).fill(0.5).map(() => {
-    const boo = Math.random() > 0.5
-    if (boo && coreCount < totalCore) {
-      coreCount++
-      return true
-    }
-    return false
-  })
-}
-createArr()
-
-// window.setInterval(() => {
-//   nodesNum = Math.ceil(Math.random() * 5)
-//   createArr()
-// }, 2000)
-
 // Nodes
 export async function loadNodes(
   doNotTriggerProgress = false,
   timeout = 20000,
 ): Promise<Array<NodeInfo>> {
-  const data = await http.get('/nodes', { doNotTriggerProgress, timeout })
-  const ret = new Array(nodesNum).fill(0).map((item, index) => {
-    const node = data[0]
-    return {
-      ...node,
-      node: `${index}-${node.node}`,
-      node_status: Math.random() > 0.5 ? 'running' : 'stopped',
-      role: arr[index] ? 'core' : 'replicant',
-      version: `5.1.${index}`,
-      // role: 'core',
-    }
-  })
-  return Promise.resolve(ret)
-  // return http.get('/nodes', { doNotTriggerProgress, timeout })
+  return http.get('/nodes', { doNotTriggerProgress, timeout })
 }
 
 export async function loadNodeDetail(node: string): Promise<NodeInfo> {
@@ -114,16 +78,7 @@ export async function loadNodeDetail(node: string): Promise<NodeInfo> {
 }
 
 export async function loadStats(): Promise<Array<NodeStatisticalData>> {
-  const data = await http.get('/stats', { doNotTriggerProgress: true })
-  const ret = new Array(nodesNum).fill(0).map((item, index) => {
-    const node = data[0]
-    return {
-      ...node,
-      node: `${index}-${node.node}`,
-    }
-  })
-  return Promise.resolve(ret)
-  // return http.get('/stats', { doNotTriggerProgress: true })
+  return http.get('/stats', { doNotTriggerProgress: true })
 }
 
 export function loadNodeStats(node: string): Promise<NodeStatisticalData> {
