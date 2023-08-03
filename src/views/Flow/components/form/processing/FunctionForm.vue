b1b56f31d0918643156dd8aa63e8fb5f6cc21981
Kinplemelon <wujiu529@gmail.com>
2023-08-03 20:09:00 +0800

    feat(flow): can switch to SQL in function form

--- src/views/Flow/components/form/processing/FunctionForm.vue
+++ src/views/Flow/components/form/processing/FunctionForm.vue
@@ -1,42 +1,72 @@
 <template>
   <div class="function-form">
-    <ul class="field-list">
-      <li class="field-item" v-for="(item, $index) in record" :key="item.id">
-        <FunctionBlock
-          v-model="record[$index]"
-          :ref="(el) => setFormCom(el, $index)"
-          :readonly="readonly"
-          @vnode-before-unmount="delFormCom($index)"
-        />
-        <el-button
-          v-if="!readonly && record.length > 1"
-          link
-          class="btn-del"
-          @click="deleteItem($index)"
-        >
-          <el-icon :size="16" class="icon-del"><Delete /></el-icon>
-        </el-button>
-      </li>
-    </ul>
-    <el-button v-if="!readonly" link type="primary" :icon="Plus" @click="addItem">
-      {{ tl('add') }}
-    </el-button>
+    <template v-if="record.editedWay === 'form'">
+      <ul class="field-list">
+        <li class="field-item" v-for="(item, $index) in record.form" :key="item.id">
+          <FunctionBlock
+            v-model="record.form[$index]"
+            :ref="(el) => setFormCom(el, $index)"
+            :readonly="readonly"
+            @vnode-before-unmount="delFormCom($index)"
+          />
+          <el-button
+            v-if="!readonly && record.form.length > 1"
+            link
+            class="btn-del"
+            @click="deleteItem($index)"
+          >
+            <el-icon :size="16" class="icon-del"><Delete /></el-icon>
+          </el-button>
+        </li>
+      </ul>
+      <el-button v-if="!readonly" link type="primary" :icon="Plus" @click="addItem">
+        {{ tl('add') }}
+      </el-button>
+    </template>
+
+    <el-form
+      v-else
+      ref="FormCom"
+      class="message-form"
+      hide-required-asterisk
+      :rules="rules"
+      :model="record"
+      :validate-on-rule-change="false"
+      @keyup.enter.prevent="saveConfig()"
+    >
+      <el-form-item prop="sql">
+        <div class="monaco-container">
+          <Monaco
+            :id="createRandomString()"
+            lang="sql"
+            v-model="record.sql"
+            @blur="transformToFormFromSql"
+          />
+        </div>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
+import { DEFAULT_SELECT } from '@/common/constants'
+import { createRandomString, trimSpacesAndLFs } from '@/common/tools'
+import Monaco from '@/components/Monaco.vue'
 import { FunctionForm } from '@/hooks/Flow/useFlowNode'
+import useGenerateFlowDataUtils from '@/hooks/Flow/useGenerateFlowDataUtils'
+import useHandleFlowDataUtils from '@/hooks/Flow/useHandleFlowDataUtils'
 import { createFunctionItem } from '@/hooks/Flow/useNodeForm'
+import useFormRules from '@/hooks/useFormRules'
 import useI18nTl from '@/hooks/useI18nTl'
 import { Delete, Plus } from '@element-plus/icons-vue'
 import { isFunction } from 'lodash'
-import { PropType, computed, defineEmits, defineExpose, defineProps } from 'vue'
+import { PropType, computed, defineEmits, defineExpose, defineProps, ref, watch } from 'vue'
 import FunctionBlock from './FunctionBlock.vue'
 
 const props = defineProps({
   modelValue: {
-    type: Array as PropType<FunctionForm>,
-    default: () => [],
+    type: Object as PropType<FunctionForm>,
+    default: () => ({ form: [] }),
   },
   readonly: {
     type: Boolean,
@@ -47,6 +77,11 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue', 'save'])
 
 const { tl } = useI18nTl('Base')
+
+const { getFuncExpressionFromFuncList } = useHandleFlowDataUtils()
+const { generateFunctionFormFromExpression } = useGenerateFlowDataUtils()
+
+const FormCom = ref()
 
 const FormComArr: Array<any> = []
 const setFormCom = (form: any, index: number) => {
@@ -66,24 +101,60 @@ const record = computed({
   },
 })
 
+const { createRequiredRule } = useFormRules()
+const rules = { sql: createRequiredRule('SQL') }
+
 const addItem = () => {
-  record.value.push(createFunctionItem())
+  record.value.form.push(createFunctionItem())
 }
 
 const deleteItem = (index: number) => {
-  record.value.splice(index, 1)
+  record.value.form.splice(index, 1)
 }
 
 const validate = () => {
-  return Promise.all(
-    FormComArr.map((item) => {
-      if (item.validate && isFunction(item.validate)) {
-        return item.validate()
-      }
-      return Promise.resolve()
-    }),
-  )
+  if (record.value.editedWay === 'form') {
+    return Promise.all(
+      FormComArr.map((item) => {
+        if (item.validate && isFunction(item.validate)) {
+          return item.validate()
+        }
+        return Promise.resolve()
+      }),
+    )
+  } else {
+    return FormCom.value.validate()
+  }
 }
+
+const transformToSqlFormForm = () => {
+  record.value.sql = getFuncExpressionFromFuncList(record.value.form) || ''
+}
+
+const transformToFormFromSql = () => {
+  const defaultForm = [createFunctionItem()]
+  if (trimSpacesAndLFs(record.value.sql) === DEFAULT_SELECT) {
+    record.value.form = [createFunctionItem()]
+  } else {
+    const form = generateFunctionFormFromExpression(record.value.sql)
+    record.value.form = form?.length ? form : defaultForm
+  }
+}
+
+const saveConfig = () => {
+  emit('save', record.value)
+}
+
+watch(
+  () => props.modelValue?.editedWay,
+  (val) => {
+    if (val === 'sql') {
+      transformToSqlFormForm()
+    } else {
+      transformToFormFromSql()
+    }
+  },
+)
 
 defineExpose({ validate })
 </script>
