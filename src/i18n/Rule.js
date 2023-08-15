65bdc50d6c43e1c8782874aedacd815a4f3863a0
LenaLenaPan <hilena.pan@gmail.com>
2023-08-15 15:50:53 +0800

    Update Rule.js

--- src/i18n/Rule.js
+++ src/i18n/Rule.js
@@ -2,45 +2,56 @@ export default {
   errorRange: {
     zh: '填写错误：数值范围为 {min} - {max}',
     en: 'Input error: the number range is {min} - {max}',
+    ja: '入力エラー：数値の範囲は {min} - {max} です',
   },
   minimumError: {
     zh: '请输入大于等于 {min} 的数',
     en: 'Please enter a number greater than or equal {min}',
+    ja: '{min} 以上の数値を入力してください',
   },
   maximumError: {
     zh: '请输入小于等于 {max} 的数',
     en: 'Please enter a number less than or equal {max}',
+    ja: '{max} 以下の数値を入力してください',
   },
   errorType: {
     zh: '类型错误：请填写{type}类型',
     en: 'Type error: please enter {type}',
+    ja: 'タイプエラー：{type}を入力してください',
   },
   errorUnit: {
     zh: '单位错误：请填写 {unit} 单位',
     en: 'Unit error: please enter {unit}',
+    ja: '単位エラー：{unit} を入力してください',
   },
   formatError: {
     zh: '请输入格式正确的值',
     en: 'Please enter the value in the correct format',
+    ja: '正しい形式で値を入力してください',
   },
   inputFieldRequiredError: {
     zh: '请输入{name}',
     en: '{name} is required.',
+    ja: '{name} を入力してください。',
   },
   selectFieldRequiredError: {
     zh: '请选择{name}',
     en: 'Please select the {name}.',
+    ja: '{name} を選択してください。',
   },
   int: {
     zh: '整数',
     en: 'integer',
+    ja: '整数',
   },
   inputRequired: {
     zh: '请输入',
     en: 'Please enter',
+    ja: '入力してください',
   },
   selectRequired: {
     zh: '请选择',
     en: 'Please select',
+    ja: '選択してください',
   },
 }
