c666363d0e784a583b53827eecdb652a4610004d
Kinplemelon <wujiu529@gmail.com>
2023-08-25 10:41:35 +0800

    fix(flow): add missing node drawer title

--- src/hooks/Flow/useNodeDrawer.ts
+++ src/hooks/Flow/useNodeDrawer.ts
@@ -1,14 +1,16 @@
+import { BridgeType } from '@/types/enum'
 import MQTTBrokerForm from '@/views/Flow/components/form/MQTTBrokerForm.vue'
 import FilterForm from '@/views/Flow/components/form/processing/FilterForm.vue'
+import FunctionForm from '@/views/Flow/components/form/processing/FunctionForm.vue'
 import EventForm from '@/views/Flow/components/form/source/EventForm.vue'
 import MessageForm from '@/views/Flow/components/form/source/MessageForm.vue'
 import BridgeHttpConfig from '@/views/RuleEngine/Bridge/Components/BridgeConfig/BridgeHttpConfig.vue'
 import ConsoleForm from '@/views/RuleEngine/components/ConsoleForm.vue'
 import RePubForm from '@/views/RuleEngine/components/RePubForm.vue'
-import FunctionForm from '@/views/Flow/components/form/processing/FunctionForm.vue'
 import { Component } from 'vue'
+import useBridgeTypeValue from '../Rule/bridge/useBridgeTypeValue'
 import useI18nTl from '../useI18nTl'
-import { ProcessingType, SinkType, SourceType } from './useFlowNode'
+import useFlowNode, { ProcessingType, SinkType, SourceType } from './useFlowNode'
 
 export default (): {
   getDrawerTitle: (type: string) => string
@@ -18,18 +20,23 @@ export default (): {
 } => {
   const { t, tl } = useI18nTl('RuleEngine')
 
+  const { isBridgeType, removeDirectionFromSpecificType } = useFlowNode()
+  const { getBridgeLabelByTypeValue } = useBridgeTypeValue()
+
   const drawerTitleMap: Record<string, string> = {
     [SourceType.Message]: tl('message'),
     [SourceType.Event]: tl('event'),
-    [SourceType.MQTTBroker]: tl('mqttBroker'),
     [ProcessingType.Function]: tl('function'),
     [ProcessingType.Filter]: t('Flow.filter'),
-    [SinkType.HTTP]: tl('HTTPServer'),
-    [SinkType.MQTTBroker]: tl('mqttBroker'),
     [SinkType.RePub]: tl('republish'),
     [SinkType.Console]: tl('consoleOutput'),
   }
-  const getDrawerTitle = (type: string) => drawerTitleMap[type] || ''
+  const getDrawerTitle = (type: string) => {
+    if (isBridgeType(type)) {
+      return getBridgeLabelByTypeValue(removeDirectionFromSpecificType(type) as BridgeType) || ''
+    }
+    return drawerTitleMap[type]
+  }
 
   const drawerDefaultWidth = '560px'
   const drawerWidthMap: Record<string, string> = {
