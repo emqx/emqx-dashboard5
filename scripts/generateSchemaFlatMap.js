ac792e70f6fa6e2381f9381ec7f711a89bc7e300
Kinplemelon <wujiu529@gmail.com>
2023-08-30 21:49:22 +0800

    try to add test

--- scripts/generateSchemaFlatMap.js
+++ scripts/generateSchemaFlatMap.js
@@ -1,33 +1,27 @@
-const regArr = [
-  /\.get/,
-  /\.put/,
-  /bridge\.node_status/,
-  /bridge\.metrics/,
-  /bridge\.node_metrics/,
-];
+const regArr = [/\.get/, /\.put/, /bridge\.node_status/, /bridge\.metrics/, /bridge\.node_metrics/]
 const removeUselessData = (obj) => {
-  const target = obj.components.schemas;
+  const target = obj.components.schemas
   for (let key in target) {
     if (regArr.some((reg) => reg.test(key))) {
-      delete target[key];
+      delete target[key]
     }
   }
-  return target;
-};
+  return target
+}
 
 const generatePropsArr = (obj) => {
-  const ret = {};
-  const keys = Object.keys(obj).sort();
+  const ret = {}
+  const keys = Object.keys(obj).sort()
   keys.forEach((key) => {
-    const singleLevelSchema = obj[key];
-    const { properties } = singleLevelSchema;
-    ret[key] = Object.keys(properties).sort();
-  });
-  return ret;
-};
+    const singleLevelSchema = obj[key]
+    const { properties } = singleLevelSchema
+    ret[key] = Object.keys(properties).sort()
+  })
+  return ret
+}
 
 const generateSchemaFlatMap = (data) => {
-  return generatePropsArr(removeUselessData(data));
+  return generatePropsArr(removeUselessData(data))
 }
 
 module.exports = generateSchemaFlatMap
---
+++ scripts/test/checkSchemasMatch.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const axios = require('axios')
+const generateSchemaFlatMap = require('../generateSchemaFlatMap')
+const bridgeSchema = require('../bridgeSchemaFlatMap.json')
+const hotConfSchema = require('../hotConfSchemaFlatMap.json')
+const { describe, expect, test } = require('jest')
+
+const [host = 'localhost', port = '18083'] = process.argv.slice(2)
+const serverPath = `http://${host}:${port}/`
+
+const updateLocalSchema = async (type) => {
+  try {
+    const { data } = await axios.get(`api/v5/schemas/${type}`, {
+      baseURL: serverPath,
+    })
+    const result = generateSchemaFlatMap(data)
+    const target = type === 'bridges' ? bridgeSchema : hotConfSchema
+    expect(result).toEqual(target)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const main = () => {
+  updateLocalSchema('bridges')
+  updateLocalSchema('hotconf')
+}
+
+main()
