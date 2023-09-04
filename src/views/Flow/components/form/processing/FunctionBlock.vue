ea6310780a8fc9b625b0bff2112cbf625c1a6ae8
Kinplemelon <wujiu529@gmail.com>
2023-09-04 17:35:43 +0800

    fix(flow): set required to `alias` after calling func

--- src/views/Flow/components/form/processing/FunctionBlock.vue
+++ src/views/Flow/components/form/processing/FunctionBlock.vue
@@ -177,8 +177,11 @@ const rules = computed(() => {
         },
       },
     ],
-    alias: [
+  }
+  if (func.name) {
+    ret.alias = [
       {
+        required: true,
         validator(rules, value, cb) {
           if (func.name && !value) {
             cb(new Error(t('Flow.aliasRequired')))
@@ -186,7 +189,7 @@ const rules = computed(() => {
           cb()
         },
       },
-    ],
+    ]
   }
   args.value.forEach((item, index) => {
     if (item.required) {
