a3c719f4e12d5758bc63b64fd698e472b3c356be
YuShifan <894402575bt@gmail.com>
2024-01-11 15:13:46 +0800

    fix(gateway): hide ws for mqtt sn

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -98,7 +98,12 @@ export const GATEWAY_DISABLED_DATABASES_MAP = {
 export const GATEWAY_DISABLED_LISTENER_TYPE_MAP: Record<string, Array<ListenerTypeForGateway>> = {
   [GatewayName.CoAP]: [ListenerTypeForGateway.TCP, ListenerTypeForGateway.SSL],
   [GatewayName.LwM2M]: [ListenerTypeForGateway.TCP, ListenerTypeForGateway.SSL],
-  [GatewayName.MQTT_SN]: [ListenerTypeForGateway.TCP, ListenerTypeForGateway.SSL],
+  [GatewayName.MQTT_SN]: [
+    ListenerTypeForGateway.TCP,
+    ListenerTypeForGateway.SSL,
+    ListenerTypeForGateway.WS,
+    ListenerTypeForGateway.WSS,
+  ],
   [GatewayName.STOMP]: [ListenerTypeForGateway.UDP, ListenerTypeForGateway.DTLS],
 }
 
