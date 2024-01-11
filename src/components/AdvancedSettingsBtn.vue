bf8581c89c7e4055db431b7d25d450d641737d7e
Kinplemelon <wujiu529@gmail.com>
2024-01-11 10:15:19 +0800

    refactor(component): optimize the `AdvancedSettingsBtn`

--- src/components/AdvancedSettingsBtn.vue
+++ src/components/AdvancedSettingsBtn.vue
@@ -1,14 +1,21 @@
 <template>
-  <el-button ref="ButtonCom" class="advanced-settings-btn" type="primary" link @click="toggle">
+  <!-- not using el-button is for not inherit the disabled state of the form -->
+  <div
+    ref="ButtonCom"
+    class="el-button el-button--primary is-link advanced-settings-btn"
+    type="primary"
+    link
+    @click="toggle"
+  >
     {{ tl('advancedSettings') }}
     <el-icon :class="{ 'is-rotate': modelValue }"><ArrowRight /></el-icon>
-  </el-button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import useI18nTl from '@/hooks/useI18nTl'
 import { ArrowRight } from '@element-plus/icons-vue'
-import { defineEmits, defineProps, onMounted, ref } from 'vue'
+import { defineEmits, defineProps } from 'vue'
 
 const props = defineProps({
   modelValue: {
@@ -23,21 +30,6 @@ const { tl } = useI18nTl('RuleEngine')
 const toggle = () => {
   emit('update:modelValue', !props.modelValue)
 }
-
-/**
- * for remove disabled prop inherit from form component...
- */
-const ButtonCom = ref()
-onMounted(() => {
-  try {
-    const el = ButtonCom.value.$el
-    el?.removeAttribute('disabled')
-    el?.removeAttribute('aria-disabled')
-    el?.classList?.remove?.('is-disabled')
-  } catch (error) {
-    console.error(error)
-  }
-})
 </script>
 
 <style lang="scss">
