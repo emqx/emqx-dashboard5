19a5a3ee797b8d4d9e910ded74001ec2cb64f91c
Kinplemelon <wujiu529@gmail.com>
2024-02-22 10:40:26 +0800

    fix(flow): should get source detail when node is input node

--- src/views/Flow/components/metrics/NodeMetrics.vue
+++ src/views/Flow/components/metrics/NodeMetrics.vue
@@ -4,7 +4,7 @@
       v-if="!infoLoading"
       :bridge-id="id"
       :bridge-msg="bridgeInfo"
-      :is-source="node?.type === FlowNodeType.Input"
+      :is-source="isSource"
       @reconnect="loadBridgeInfo"
     />
   </div>
@@ -12,6 +12,7 @@
 
 <script setup lang="ts">
 import useHandleActionItem from '@/hooks/Rule/action/useHandleActionItem'
+import useHandleSourceItem from '@/hooks/Rule/action/useHandleSourceItem'
 import { FlowNodeType } from '@/hooks/Flow/useFlowNode'
 import { useBridgeDataHandler } from '@/hooks/Rule/useDataHandler'
 import BridgeItemOverview from '@/views/RuleEngine/Bridge/Components/BridgeItemOverview.vue'
@@ -29,13 +30,17 @@ const id = computed(() => {
 const bridgeInfo: Ref<BridgeItem> = ref({} as BridgeItem)
 const infoLoading = ref(false)
 
+const isSource = computed(() => props?.node?.type === FlowNodeType.Input)
+
 const { handleBridgeDataAfterLoaded } = useBridgeDataHandler()
-const { getDetail } = useHandleActionItem()
+const { getDetail: getActionDetail } = useHandleActionItem()
+const { getSourceDetail } = useHandleSourceItem()
 
 const loadBridgeInfo = async () => {
   infoLoading.value = true
   try {
-    const data = await getDetail(id.value)
+    const request = isSource.value ? getSourceDetail : getActionDetail
+    const data = await request(id.value)
     bridgeInfo.value = handleBridgeDataAfterLoaded(data)
   } catch (error) {
     console.error(error)
