3af27458d3e1a31c5af883a2649326b16188f839
YuShifan <894402575bt@gmail.com>
2024-03-29 10:21:17 +0800

    feat(plugin): completed the initial data construction

---
+++ src/views/Plugins/components/PluginManage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="plugin-manage">Plugin Manage</div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+import usePluginRenderForm from '@/hooks/Plugins/usePluginRenderForm'
+
+const props = defineProps({
+  pluginName: {
+    type: String,
+    required: true,
+  },
+})
+
+usePluginRenderForm({ name: props.pluginName })
+</script>
