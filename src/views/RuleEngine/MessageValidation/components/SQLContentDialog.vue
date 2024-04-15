eb565ba366d73091dcf525644bea7251bed52a90
Kinplemelon <wujiu529@gmail.com>
2024-04-15 14:41:05 +0800

    refactor(message validation): adjusted form

---
+++ src/views/RuleEngine/MessageValidation/components/SQLContentDialog.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-dialog
+    v-model="showDialog"
+    :width="500"
+    class="common-dialog"
+    :title="tl('pleaseEnterSQL')"
+    destroy-on-close
+  >
+    <el-form ref="FormCom" label-position="top" :rules="rules" :model="form" hide-required-asterisk>
+      <el-form-item prop="sql">
+        <div class="monaco-container">
+          <monaco id="database-filter" v-model="form.sql" lang="sql" />
+        </div>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-align-footer">
+        <el-button @click="showDialog = false">
+          {{ t('Base.cancel') }}
+        </el-button>
+        <el-button type="primary" @click="submit">
+          {{ t('Base.confirm') }}
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import Monaco from '@/components/Monaco.vue'
+import useFormRules from '@/hooks/useFormRules'
+import useI18nTl from '@/hooks/useI18nTl'
+import { computed, defineEmits, defineProps, ref, watch } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true,
+  },
+  sql: {
+    type: String,
+  },
+})
+const emit = defineEmits<{
+  (e: 'update:modelValue', v: boolean): void
+  (e: 'submit', v: string): void
+}>()
+
+const { t, tl } = useI18nTl('RuleEngine')
+
+const showDialog = computed({
+  get: () => props.modelValue,
+  set: (val: boolean) => {
+    emit('update:modelValue', val)
+  },
+})
+
+watch(showDialog, (val) => {
+  if (!val) {
+    form.value.sql = ''
+  } else {
+    form.value.sql = props.sql || ''
+  }
+})
+
+const FormCom = ref()
+
+const form = ref({ sql: '' })
+
+const { createRequiredRule } = useFormRules()
+
+const rules = { sql: createRequiredRule('SQL') }
+
+const submit = async () => {
+  try {
+    await FormCom.value.validate()
+    emit('submit', form.value.sql)
+    showDialog.value = false
+  } catch (error: any) {
+    //
+  }
+}
+</script>
