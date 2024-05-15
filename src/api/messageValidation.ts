77d527073a72b95c5c9542ba98049ab819bdbd41
Kinplemelon <wujiu529@gmail.com>
2024-05-15 11:01:27 +0800

    refactor(schema validation): update api path

--- src/api/messageValidation.ts
+++ src/api/messageValidation.ts
@@ -8,37 +8,37 @@ import {
 export const postMessageValidation = (
   validation: MessageValidation,
 ): Promise<MessageValidation> => {
-  return http.post(`/message_validations`, validation)
+  return http.post(`/schema_validations`, validation)
 }
 
 export const getMessageValidations = (): Promise<Array<MessageValidation>> => {
-  return http.get(`/message_validations`)
+  return http.get(`/schema_validations`)
 }
 
 export const putMessageValidation = (validation: MessageValidation): Promise<MessageValidation> => {
-  return http.put(`/message_validations`, validation)
+  return http.put(`/schema_validations`, validation)
 }
 
 export const deleteValidation = (name: string): Promise<void> => {
-  return http.delete(`/message_validations/validation/${name}`)
+  return http.delete(`/schema_validations/validation/${name}`)
 }
 
 export const getMessageValidationDetail = (name: string): Promise<MessageValidation> => {
-  return http.get(`/message_validations/validation/${name}`)
+  return http.get(`/schema_validations/validation/${name}`)
 }
 
 export const enableDisableValidation = (name: string, enable: boolean): Promise<void> => {
-  return http.post(`/message_validations/validation/${name}/enable/${enable}`)
+  return http.post(`/schema_validations/validation/${name}/enable/${enable}`)
 }
 
 export const getValidationMetrics = (name: string): Promise<MessageValidationMetrics> => {
-  return http.get(`/message_validations/validation/${name}/metrics`)
+  return http.get(`/schema_validations/validation/${name}/metrics`)
 }
 
 export const resetValidationMetrics = (name: string): Promise<void> => {
-  return http.post(`/message_validations/validation/${name}/metrics/reset`)
+  return http.post(`/schema_validations/validation/${name}/metrics/reset`)
 }
 
 export const reorderAllValidations = (validation: ReorderValidationParams): Promise<void> => {
-  return http.post(`/message_validations/reorder`, validation)
+  return http.post(`/schema_validations/reorder`, validation)
 }
