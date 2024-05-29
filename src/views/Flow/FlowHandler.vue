d7f9e6b966fdcfaff2f94e165948eea27885ebd7
Kinplemelon <wujiu529@gmail.com>
2024-05-29 14:50:38 +0800

    chore(package): upgrade vue for fix warning

--- src/views/Flow/FlowHandler.vue
+++ src/views/Flow/FlowHandler.vue
@@ -31,7 +31,6 @@
         v-if="editingMethod === EditingMethod.Flow"
         :data="flowData"
       />
-      <SQLEditor v-if="editingMethod === EditingMethod.SQL" />
     </div>
   </div>
   <FlowNameDialog
@@ -57,7 +56,6 @@ import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import FlowEditor from './components/FlowEditor.vue'
 import FlowNameDialog from './components/FlowNameDialog.vue'
-import SQLEditor from './components/SQLEditor.vue'
 
 interface FlowBasicInfo {
   name: string
