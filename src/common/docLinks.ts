cf273c7d0afbcfce54e7f55cf429bb0476b7d7b6
Kinplemelon <wujiu529@gmail.com>
2024-06-06 17:21:19 +0800

    refactor(rule): remove old `EventsSelect` component

--- src/common/docLinks.ts
+++ src/common/docLinks.ts
@@ -6,9 +6,6 @@ type DocKey =
   | 'documentation'
   | 'forum'
   | 'gitHub'
-  | 'ruleEvent'
-  | 'bridgeAsFrom'
-  | 'ruleEventMsgPub'
   | 'upgrade'
   | 'dashboard'
   | 'mqttStudy'
@@ -67,13 +64,6 @@ const createDocLinks = (lang: string): DocMap => {
     documentation: `https://docs.emqx.com/${lang}/emqx/${EMQX_VERSION}/?${QUERY_FOR_HELP}`,
     forum: lang === 'en' ? `https://forum.emqx.io/` : `https://askemq.com/`,
     gitHub: `https://github.com/emqx/emqx`,
-    ruleEvent: `https://docs.emqx.com/${lang}/emqx/${EMQX_VERSION}/data-integration/rule-sql-events-and-fields.html`,
-    bridgeAsFrom: `https://docs.emqx.com/${lang}/emqx/${EMQX_VERSION}/data-integration/rule-sql-events-and-fields.html#${
-      lang === 'zh' ? '数据桥接' : 'data-bridges'
-    }`,
-    ruleEventMsgPub: `https://docs.emqx.com/${lang}/emqx/${EMQX_VERSION}/data-integration/rule-sql-events-and-fields.html#${
-      lang === 'zh' ? 'mqtt-消息' : 'mqtt-message'
-    }`,
     upgrade: `https://www.emqx.com/${lang}/lp/upgrade-emqx/enterprise?${QUERY_FOR_GO_UPGRADE}`,
     blog: `https://www.emqx.com/${lang}/blog/category/emqx?${QUERY_FOR_HELP}`,
     dashboard: `https://docs.emqx.com/${lang}/emqx/${EMQX_VERSION}/dashboard/introduction.html?${QUERY_FOR_HELP}`,
