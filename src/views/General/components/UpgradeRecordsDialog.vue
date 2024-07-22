c9d8ed2d4d55aae565a1db132ec14f53235330d1
Kinplemelon <wujiu529@gmail.com>
2024-07-22 10:21:56 +0800

    feat(hot upgrade): request api

---
+++ src/views/General/components/UpgradeRecordsDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-dialog
+    :title="tl('upgradeRecords')"
+    v-model="showDialog"
+    class="records-dialog"
+    width="75%"
+    destroy-on-close
+  >
+    <el-table :data="list">
+      <el-table-column :label="tl('startedAt')">
+        <template #default="{ row }">
+          {{ moment(row.started_at).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+      </el-table-column>
+      <el-table-column :label="tl('finishedAt')">
+        <template #default="{ row }">
+          {{ moment(row.finished_at).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="from_vsn" :label="tl('fromVsn')" />
+      <el-table-column prop="target_vsn" :label="tl('targetVsn')" />
+      <el-table-column :label="t('Base.status')">
+        <template #default="{ row }">
+          {{ getNodeStatusLabel(row.status) }}
+        </template>
+      </el-table-column>
+      <el-table-column :label="t('Clients.result')">
+        <template #default="{ row }">
+          <span>{{ getResultLabel(row.result) }}</span>
+          <InfoTooltip v-if="!isSucResult(row.result)" :content="row.result" />
+        </template>
+      </el-table-column>
+    </el-table>
+    <template #footer>
+      <el-button @click="showDialog = false">
+        {{ t('APIKey.close') }}
+      </el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import InfoTooltip from '@/components/InfoTooltip.vue'
+import useI18nTl from '@/hooks/useI18nTl'
+import { NodeUpgradeData } from '@/types/typeAlias'
+import moment from 'moment'
+import { computed, defineEmits, defineProps } from 'vue'
+
+const props = defineProps<{ modelValue: boolean; node?: NodeUpgradeData }>()
+const emit = defineEmits(['update:modelValue'])
+
+const showDialog = computed({
+  get: () => props.modelValue,
+  set: (val: boolean) => {
+    emit('update:modelValue', val)
+  },
+})
+
+const { t, tl } = useI18nTl('General')
+
+const list = computed(() => props?.node?.upgrade_history || [])
+
+const isUpgrading = (status: string) => status === 'in-progress'
+
+const getNodeStatusLabel = (status: string) =>
+  isUpgrading(status) ? tl('upgrading') : tl('finished')
+
+const isSucResult = (result: string | Record<string, any>) => result === 'success'
+const getResultLabel = (result: string | Record<string, any>) =>
+  isSucResult(result) ? t('Base.success') : t('Base.failed')
+</script>
