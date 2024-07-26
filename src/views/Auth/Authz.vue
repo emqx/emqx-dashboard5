53e41fffe17422fa4154191b53014bb8b4169e99
Kinplemelon <wujiu529@gmail.com>
2024-07-26 16:28:33 +0800

    refactor(authz): support config `max_rules` in type built in data base

--- src/views/Auth/Authz.vue
+++ src/views/Auth/Authz.vue
@@ -58,7 +58,6 @@
             :row-data="row"
             :table-data-len="authzList.length"
             :position="$index"
-            :type="row.type"
             @delete="handleDelete"
             @setting="routeToDetail"
             @move-up="moveAuthzUp($index)"
