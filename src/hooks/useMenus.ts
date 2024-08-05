7606e464d6c2aac43f27d0046a02a6d016604ac1
Kinplemelon <wujiu529@gmail.com>
2024-08-05 17:13:57 +0800

    Revert "fix(msg transform): hide msg transform first"
    This reverts commit 8ac5114a764500e769c1a304157cd94772c7642f.

--- src/hooks/useMenus.ts
+++ src/hooks/useMenus.ts
@@ -61,7 +61,7 @@ export default (): {
     { title: 'connector', path: '/connector' },
     { title: 'schema-validation', path: '/schema-validation' },
     { title: 'schema', path: '/schema' },
-    // { title: 'message-transform', path: '/message-transform' },
+    { title: 'message-transform', path: '/message-transform' },
   ]
 
   const diagnose = [
