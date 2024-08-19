dd3845127a08d5d786bbd85067396405d95d4866
Kinplemelon <wujiu529@gmail.com>
2024-08-19 15:32:54 +0800

    feat(gateway): support config `health_check` in CoAP listener

--- src/i18n/Gateway.js
+++ src/i18n/Gateway.js
@@ -360,4 +360,20 @@ export default {
     zh: '使用 Hocon 配置更多监听器参数。',
     en: 'Use hocon to configure more listener parameters.',
   },
+  healthCheck: {
+    zh: '健康检查',
+    en: 'Health Check',
+  },
+  healthCheckDesc: {
+    zh: '健康检查请求内容',
+    en: 'Health Check Request Content',
+  },
+  healthCheckResponse: {
+    zh: '健康检查响应',
+    en: 'Health Check Response',
+  },
+  healthCheckResponseDesc: {
+    zh: '健康检查响应内容',
+    en: 'Health Check Response Content',
+  },
 }
