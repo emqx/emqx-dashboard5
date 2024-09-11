c27cca63d2335b08f01f45d748657626115fe37e
Kinplemelon <wujiu529@gmail.com>
2024-09-11 16:46:45 +0800

    refactor(scripts): use esm

--- scripts/checkPackageVersion.cjs
+++ scripts/checkPackageVersion.cjs
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const packageJSON = require('../package.json')
 const axios = require('axios')
-const baseURL = import.meta.env?.HOST_URL || 'http://localhost:18083'
+const { loadEnv } = require('vite')
+
+const envVariables = loadEnv('development', process.cwd(), '')
+const baseURL = envVariables.HOST_URL || 'http://localhost:18083'
+
 const rawPackageVersion = packageJSON.version
 
 const packageVersionRegex = /^(\d+\.\d+)/
