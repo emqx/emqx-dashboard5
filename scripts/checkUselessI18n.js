a2c329087a71b287a12421c13c2f07371f0d0e3b
Kinplemelon <wujiu529@gmail.com>
2024-09-11 16:46:45 +0800

    refactor(scripts): use esm

--- scripts/checkUselessI18n.js
+++ scripts/checkUselessI18n.js
@@ -1,15 +1,23 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs')
-const { execSync } = require('child_process')
-const path = require('path')
+import fs from 'fs/promises'
+import { execSync } from 'child_process'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const useLessKey = {}
 
-const scanFile = (filePath) => {
+const scanFile = async (filePath) => {
   console.log(`Checking ${filePath}`)
-  const { default: json_data } = require('esm')(module)(`../${filePath}`)
+  const fullPath = path.resolve(__dirname, '..', filePath)
 
-  Object.keys(json_data).forEach((key) => {
+  // Dynamically import the module
+  const module = await import(fullPath)
+  const json_data = module.default || module
+
+  for (const key of Object.keys(json_data)) {
     try {
       execSync(`grep -r --exclude-dir=i18n -E '${key}[^\\w]' ./src`, {
         encoding: 'utf-8',
@@ -20,22 +28,20 @@ const scanFile = (filePath) => {
       }
       useLessKey[filePath].push(key)
     }
-  })
+  }
 }
 
-function traverseDir(dirPath) {
-  fs.readdir(dirPath, (err, files) => {
-    if (err) {
-      console.error(`Failed to read directory ${dirPath}: ${err}`)
-      return
-    }
-
-    files.forEach((file) => {
+async function traverseDir(dirPath) {
+  try {
+    const files = await fs.readdir(dirPath)
+    for (const file of files) {
       const filePath = path.join(dirPath, file)
-      scanFile(filePath)
-    })
+      await scanFile(filePath)
+    }
     console.log(useLessKey)
-  })
+  } catch (err) {
+    console.error(`Failed to read directory ${dirPath}: ${err}`)
+  }
 }
 
-traverseDir('./src/i18n')
+traverseDir('./src/i18n').catch(console.error)
