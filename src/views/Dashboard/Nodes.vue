6cc881f611c4a5311a8d4a33e627e7764b63f053
Kinplemelon <wujiu529@gmail.com>
2024-10-28 15:10:30 +0800

    feat(nodes): display connections in the node list

--- src/views/Dashboard/Nodes.vue
+++ src/views/Dashboard/Nodes.vue
@@ -7,7 +7,7 @@
       </el-button>
     </div>
     <el-table :data="nodes" v-loading.lock="nodesLockTable" class="nodes-table">
-      <el-table-column prop="node" :label="tl('name')">
+      <el-table-column prop="node" :label="tl('name')" min-width="160">
         <template #default="{ row }">
           <router-link class="node-name" :to="`nodes/${row.node}`">{{ row.node }}</router-link>
         </template>
@@ -23,13 +23,14 @@
           </span>
         </template>
       </el-table-column>
-      <el-table-column prop="uptime" :label="tl('uptime')">
+      <el-table-column prop="uptime" :label="tl('uptime')" min-width="160">
         <template #default="{ row }">
           {{ transMsNumToSimpleStr(row.uptime) }}
         </template>
       </el-table-column>
       <el-table-column prop="version" :label="tl('version')" width="90"> </el-table-column>
-      <el-table-column :label="`Erlang ${tl('process')}`">
+      <el-table-column prop="connections" :label="t('Clients.connect')" min-width="120" />
+      <el-table-column :label="`Erlang ${tl('process')}`" min-width="160">
         <template #default="{ row }">
           <el-tooltip
             placement="top"
@@ -45,7 +46,7 @@
           </el-tooltip>
         </template>
       </el-table-column>
-      <el-table-column v-if="hasMemory" :label="tl('memory')" prop="memory_used">
+      <el-table-column v-if="hasMemory" :label="tl('memory')" prop="memory_used" min-width="160">
         <template #default="{ row }">
           <el-tooltip
             placement="top"
@@ -61,7 +62,7 @@
           </el-tooltip>
         </template>
       </el-table-column>
-      <el-table-column :label="tl('osCpuLoad')">
+      <el-table-column :label="tl('osCpuLoad')" min-width="120">
         <template #default="{ row }">
           <el-tooltip class="box-item" effect="dark" content="load1/load5/load15" placement="top"
             >{{ row.load1 }}/{{ row.load5 }}/{{ row.load15 }}
@@ -82,16 +83,12 @@ export default defineComponent({
 
 <script setup lang="ts">
 import { caseInsensitiveCompare, calcPercentage } from '@/common/tools'
-import { useI18n } from 'vue-i18n'
+import useI18nTl from '@/hooks/useI18nTl'
 import { RefreshRight } from '@element-plus/icons-vue'
 import useDurationStr from '@/hooks/useDurationStr'
 import useClusterNodes from '@/hooks/useClusterNodes'
 
-const { t } = useI18n()
-
-const tl = function (key: string, collection = 'Dashboard') {
-  return t(collection + '.' + key)
-}
+const { t, tl } = useI18nTl('Dashboard')
 
 const { transMsNumToSimpleStr } = useDurationStr()
 
