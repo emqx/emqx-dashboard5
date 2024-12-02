04e2c3f750adac5abbbab317272e826de3ffb378
Kinplemelon <wujiu529@gmail.com>
2024-12-02 22:58:40 +0800

    style(stream): adjust style from ui designer

--- src/views/Streaming/StreamDetail.vue
+++ src/views/Streaming/StreamDetail.vue
@@ -3,34 +3,35 @@
     <div class="block-header">
       <DetailHeader :item="{ name: streamName, routeName: 'stream' }" />
     </div>
-    <el-row :gutter="26">
-      <el-col :span="12">
-        <el-card class="basic-info top-border">
-          <el-descriptions :title="t('Dashboard.basic')" border :column="1" size="large">
-            <el-descriptions-item :label="tl('streamName')">
-              {{ streamInfo.overview?.stream_name }}
-            </el-descriptions-item>
-            <el-descriptions-item :label="tl('streamType')">
-              {{ tl(`streamTypeLabel.${streamInfo.overview?.stream_type}`) }}
-            </el-descriptions-item>
-            <el-descriptions-item :label="tl('partitionNum')">
-              {{ streamInfo.overview?.partition_number }}
-            </el-descriptions-item>
-            <el-descriptions-item :label="tl('mqttTopic')">
-              {{ streamInfo.overview?.mqtt_topic_filter }}
-            </el-descriptions-item>
-            <el-descriptions-item :label="tl('retention')">
-              {{ transMsNumToSimpleStr(streamInfo.overview?.retention_time) }}
-            </el-descriptions-item>
-          </el-descriptions>
-        </el-card>
-      </el-col>
-    </el-row>
+    <div>
+      <div class="section-header is-first">
+        <p>{{ t('Dashboard.basic') }}</p>
+      </div>
+      <el-card class="basic-info no-border" v-loading="isLoading">
+        <el-descriptions :column="2" size="large">
+          <el-descriptions-item :label="tl('streamName')" width="50%">
+            {{ streamInfo.overview?.stream_name }}
+          </el-descriptions-item>
+          <el-descriptions-item :label="tl('streamType')" width="50%">
+            {{ tl(`streamTypeLabel.${streamInfo.overview?.stream_type}`) }}
+          </el-descriptions-item>
+          <el-descriptions-item :label="tl('partitionNum')">
+            {{ streamInfo.overview?.partition_number }}
+          </el-descriptions-item>
+          <el-descriptions-item :label="tl('mqttTopic')">
+            {{ streamInfo.overview?.mqtt_topic_filter }}
+          </el-descriptions-item>
+          <el-descriptions-item :label="tl('retention')">
+            {{ transMsNumToSimpleStr(streamInfo.overview?.retention_time) }}
+          </el-descriptions-item>
+        </el-descriptions>
+      </el-card>
+    </div>
     <div>
       <div class="section-header">
         <p>{{ tl('partitions') }}</p>
       </div>
-      <el-table :data="streamInfo.partitions || []">
+      <el-table :data="streamInfo.partitions || []" v-loading="isLoading">
         <el-table-column prop="partition_index" :label="tl('partition')" />
         <el-table-column prop="start_offset" :label="tl('startOffset')" />
         <el-table-column prop="end_offset" :label="tl('endOffset')" />
@@ -92,8 +93,8 @@ const { transMsNumToSimpleStr } = useDurationStr()
 <style lang="scss">
 .stream-detail {
   padding-bottom: 32px;
-  .basic-info:before {
-    background: linear-gradient(135deg, #00b173 0%, #009580 100%);
+  .section-header.is-first {
+    margin-top: 8px;
   }
 }
 </style>
