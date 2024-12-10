e207f843f065f67658b3dbc7d3b57a69faed04da
Kinplemelon <wujiu529@gmail.com>
2024-12-10 17:03:35 +0800

    fix(action): style of detail tooltip

--- src/components/Metrics/TypeMetricsItem.vue
+++ src/components/Metrics/TypeMetricsItem.vue
@@ -156,6 +156,11 @@ watch(
     }
   }
 }
+.el-popper:not(.el-select__popper):not(.el-picker__popper):not(.is-wider):not(.el-cascader__dropdown):not(.el-autocomplete__popper) {
+  &.type-detail-tooltip {
+    max-width: 700px;
+  }
+}
 .metrics-details {
   padding: 16px;
   .el-descriptions {
