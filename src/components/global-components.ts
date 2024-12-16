6a7c5503b6b4c10a267119add8179a694a532eb0
Kinplemelon <wujiu529@gmail.com>
2024-12-16 11:15:11 +0800

    style(table): style of table

--- src/components/global-components.ts
+++ src/components/global-components.ts
@@ -6,3 +6,12 @@ export default {
     app.component('CustomInputPassword', CustomInputPassword)
   },
 }
+
+export const setPropsToElComponents = {
+  install(app: App): void {
+    const elTable = app?._context?.components?.ElTable as any
+    if (elTable && elTable.props) {
+      elTable.props.stripe = { type: Boolean, default: true }
+    }
+  },
+}
