ea91c3b5f89cfad61376c3c5c45d3800d288264d
Kinplemelon <wujiu529@gmail.com>
2025-01-13 15:12:50 +0800

    fix(authn): validate filter in mongodb config

--- src/views/Auth/AuthnCreate.vue
+++ src/views/Auth/AuthnCreate.vue
@@ -128,7 +128,7 @@
             v-if="['mysql', 'postgresql', 'mongodb', 'redis'].includes(backend)"
             v-model="configData"
             ref="formCom"
-            :database="backend as DatabaseAndServerDOM"
+            :database="(backend as DatabaseAndServerDOM)"
             auth-type="authn"
           />
           <built-in-config
