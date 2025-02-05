859a18b6cb8c5cf119b29af5e5bc15f70686e313
Kinplemelon <wujiu529@gmail.com>
2025-02-05 14:27:53 +0800

    refactor(sso): add desc for configuration file

--- src/views/Base/components/TongTechAuthGuidance.vue
+++ src/views/Base/components/TongTechAuthGuidance.vue
@@ -9,16 +9,26 @@
           <el-button link type="primary">{{ lowerCase(t('Base.add')) }}</el-button>
         </el-tooltip>
       </template>
+      <template #configFile>
+        <el-tooltip placement="top" popper-class="is-wider auth-config-tooltip">
+          <template #content>
+            <MarkdownContent :content="configFileDesc" in-tooltip />
+          </template>
+          <el-button link type="primary">{{ lowerCase(t('Base.configFile')) }}</el-button>
+        </el-tooltip>
+      </template>
     </i18n-t>
   </div>
 </template>
 
 <script setup lang="ts">
 import CodeView from '@/components/CodeView.vue'
+import MarkdownContent from '@/components/MarkdownContent.vue'
 import { lowerCase } from 'lodash'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { t } = useI18n()
+const { t, locale } = useI18n()
 const dashboard_addr = location.origin + location.pathname.slice(0, -1)
 const configView = `dashboard {
   sso {
@@ -27,12 +37,36 @@ const configView = `dashboard {
       enable = true
       backend = tongauth
       auth_server_addr = "http://168.1.15.162:11111"
-      dashboard_addr = "${dashboard_addr}"
+      dashboard_addr = "${dashboard_addr}"AI
       client_id = tongmmp_yunjian_oauth2
       client_secret = "b2c3d4e5-f678-9c0a-1b2f-4567890abcde"
     }
   }
 }`
+
+const zhConfigFileDesc = `配置文件目录位置
+| 安装方式               | 路径                     |
+| ---------------------- | ------------------------ |
+| 使用 RPM 或 DEB 包安装 |  \`/var/lib/emqx/configs \`  |
+| 在 Docker 容器中运行   |  \`/opt/emqx/data/configs \` |
+| 从便携式压缩包中提取   |  \`./data/configs \`         |
+
+目录下的 \`cluster.hocon\` 文件
+`
+
+const enConfigFileDesc = `The location of the configuration file directory
+| Installation                               | Path                     |
+| ------------------------------------------ | ------------------------ |
+| Installed with RPM or DEB package          | \`/var/lib/emqx/configs\`  |
+| Running in docker container                | \`/opt/emqx/data/configs\` |
+| Extracted from portable compressed package | \`./data/configs\`         |
+
+The \`cluster.hocon\` file in the directory
+`
+
+const configFileDesc = computed(() => {
+  return locale.value === 'zh' ? zhConfigFileDesc : enConfigFileDesc
+})
 </script>
 
 <style lang="scss">
@@ -47,6 +81,7 @@ const configView = `dashboard {
     vertical-align: baseline;
     font-size: 16px;
     font-weight: normal;
+    margin-left: 0;
   }
 }
 .auth-config-tooltip {
