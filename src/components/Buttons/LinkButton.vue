e40a09d4c1ca02a2c58c1e35e021a6bc13ebdf62
Kinplemelon <wujiu529@gmail.com>
2025-02-18 11:49:18 +0800

    feat(component): link button

---
+++ src/components/Buttons/LinkButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <component
+    :is="disabled ? 'el-button' : 'router-link'"
+    class="el-button"
+    :to="to"
+    :class="{ 'is-disabled': disabled }"
+  >
+    <el-icon v-if="icon" :size="14">
+      <component :is="icon" />
+    </el-icon>
+    <span v-if="icon">
+      <slot />
+    </span>
+    <slot v-else />
+  </component>
+</template>
+
+<script setup lang="ts">
+import type { Component } from 'vue'
+import { defineProps } from 'vue'
+
+defineProps<{
+  to: string
+  icon?: Component
+  disabled?: boolean
+}>()
+</script>
