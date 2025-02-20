a95bafa9d59aa783102ad5b676d39dc369684dff
Kinplemelon <wujiu529@gmail.com>
2025-02-20 17:51:21 +0800

    chore(package): initial import of auto import dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import autoImportConfig from './auto-import.config.js'
 import { version as packageVersion } from './package.json'
 
 const getVersion = (packageVersion) => {
@@ -29,6 +31,7 @@ export default defineConfig(({ mode }) => {
           util: true,
         },
       }),
+      AutoImport(autoImportConfig),
     ],
     server: {
       port: 7000,
