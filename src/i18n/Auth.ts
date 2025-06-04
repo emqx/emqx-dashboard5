efecdf152124b1ea0acae66e872dc55ef9f066b0
Kinplemelon <wujiu529@gmail.com>
2025-06-04 14:49:20 +0800

    fix(authz): config desc

--- src/i18n/Auth.ts
+++ src/i18n/Auth.ts
@@ -340,8 +340,8 @@ export default {
     en: 'Pattern',
   },
   permissionListenerDesc: {
-    zh: '用于过滤应用规则的监听器，由监听器类型和名称组成，`{监听器类型}:{监听器名称}`。例如：`tcp:default`，`ws:default`',
-    en: 'Listener for filtering applied rules, composed of listener type and name, `{listener type}:{listener name}`. For example: `tcp:default`, `ws:default`',
+    zh: "用于过滤应用规则的监听器，由监听器类型和名称组成，`{'{'}监听器类型{'}'}:{'{'}监听器名称{'}'}`。例如：`tcp:default`，`ws:default`",
+    en: "Listener for filtering applied rules, composed of listener type and name, `{'{'}listener type{'}'}:{'{'}listener name{'}'}`. For example: `tcp:default`, `ws:default`",
   },
   permissionListenerRegexDesc: {
     zh: '用于过滤应用规则的监听器正则表达式。例如：`^tcp:.$`，`^ws:.$`',
