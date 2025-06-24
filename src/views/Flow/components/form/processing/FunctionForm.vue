fe4d2ec570ddabad0fcdd627320e9746e21b847e
Kinplemelon <wujiu529@gmail.com>
2025-06-24 17:50:40 +0800

    fix(flow): only validate the format when the alias has a value

--- src/views/Flow/components/form/processing/FunctionForm.vue
+++ src/views/Flow/components/form/processing/FunctionForm.vue
@@ -197,7 +197,7 @@ const rules: Rules = {
       const errors = []
       if (source.func.name && !value) {
         errors.push(new Error(t('Flow.aliasRequired')))
-      } else if (!correctAliasReg.test(value)) {
+      } else if (value && !correctAliasReg.test(value)) {
         errors.push(new Error(t('Flow.aliasFormatError')))
       }
       return errors
