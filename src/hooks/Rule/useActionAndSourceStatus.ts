acd154d3994796471f155a9d45a99fe7477ccc48
Kinplemelon <wujiu529@gmail.com>
2025-08-15 18:21:43 +0800

    refactor(flow): remove unused filter connector hook and replace action status logic with shared implementation

--- src/hooks/Rule/useActionAndSourceStatus.ts
+++ src/hooks/Rule/useActionAndSourceStatus.ts
@@ -1,31 +1,3 @@
-import { ConnectionStatus } from '@/types/enum'
-
-const useActionAndSourceStatus = (): {
-  statusOptList: Array<{ value: ConnectionStatus; label: string }>
-  statusLabelMap: Partial<Record<ConnectionStatus, string>>
-  getActionStatusLabel: (status?: ConnectionStatus) => string
-} => {
-  const { t, tl } = useI18nTl('RuleEngine')
-  const statusLabelMap: Partial<Record<ConnectionStatus, string>> = {
-    [ConnectionStatus.Connected]: tl('actionAvailable'),
-    [ConnectionStatus.Disconnected]: tl('actionUnavailable'),
-    [ConnectionStatus.Connecting]: t('Base.connecting'),
-    [ConnectionStatus.Inconsistent]: t('Base.inconsistent'),
-  }
-  const statusOptList = (Object.entries(statusLabelMap) as [ConnectionStatus, string][]).map(
-    ([key, value]) => ({
-      value: key,
-      label: value,
-    }),
-  )
-  const getActionStatusLabel = (status?: ConnectionStatus) => {
-    return status ? statusLabelMap[status] || tl('disconnected') : ''
-  }
-  return {
-    statusLabelMap,
-    statusOptList,
-    getActionStatusLabel,
-  }
-}
+import { useActionAndSourceStatus } from '@emqx/shared-ui-components'
 
 export default useActionAndSourceStatus
