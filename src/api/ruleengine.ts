cd4c4530462344034e3df9fbba2660a755d345ec
Kinplemelon <wujiu529@gmail.com>
2025-09-11 10:30:43 +0800

    refactor(rule engine): remove bridge-related functions and update resource dictionary

--- src/api/ruleengine.ts
+++ src/api/ruleengine.ts
@@ -2,14 +2,12 @@ import http from '@/common/http'
 import type { ListDataWithPagination } from '@/types/common'
 import type { LicenseData } from '@/types/dashboard'
 import type {
-  BridgeItem,
-  BridgeMetricsData,
+  GenerateSQLPayload,
+  GenerateSQLResponse,
   ParamsForQueryRules,
   RuleItem,
   RuleMetrics,
   SchemaRegistry,
-  GenerateSQLPayload,
-  GenerateSQLResponse,
 } from '@/types/rule'
 import type {
   ExternalSchema,
@@ -17,89 +15,6 @@ import type {
   SchemaRegistryDetail,
   SchemaRegistryProtobufBundle,
 } from '@/types/typeAlias'
-
-//Bridges
-export async function getBridgeList(): Promise<any> {
-  try {
-    const data = await http.get('/bridges')
-    return Promise.resolve(
-      data.map((item: Omit<BridgeItem, 'id' | 'idForRuleFrom'>) => {
-        const id = getBridgeKey(item)
-        return {
-          id,
-          idForRuleFrom: `${RULE_INPUT_BRIDGE_TYPE_PREFIX}${id}`,
-          ...item,
-        }
-      }),
-    )
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
-
-export async function createBridge(body: Record<string, unknown>): Promise<BridgeItem> {
-  try {
-    const data = await http.post('/bridges', body)
-    return Promise.resolve({ ...data, id: getBridgeKey(data) })
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
-
-export async function updateBridge(id: string, body: BridgeItem): Promise<any> {
-  if (!id) return Promise.reject()
-  try {
-    const data = await http.put('/bridges/' + encodeURIComponent(id), body)
-    return Promise.resolve({ ...data, id: getBridgeKey(data) })
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
-
-export function startStopBridge(id: string, isEnable: boolean): Promise<any> {
-  if (!id) return Promise.reject()
-  return http.put(`/bridges/${encodeURIComponent(id)}/enable/${isEnable}`)
-}
-
-export async function getBridgeInfo(id: string): Promise<any> {
-  if (!id) return Promise.reject()
-  try {
-    const data = await http.get('/bridges/' + encodeURIComponent(id))
-    return Promise.resolve({ ...data, id: getBridgeKey(data) })
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
-
-export function deleteBridge(id: string, withDependency = false): Promise<any> {
-  if (!id) return Promise.reject()
-  return http.delete(
-    `/bridges/${encodeURIComponent(id)}${withDependency ? '?also_delete_dep_actions' : ''}`,
-    {
-      errorsHandleCustom: [400],
-    },
-  )
-}
-
-export function reconnectBridge(bridgeID: string): Promise<void> {
-  return http.post(`/bridges/${bridgeID}/start`)
-}
-
-export function reconnectBridgeForNode(node: string, bridgeID: string): Promise<void> {
-  return http.post(`/nodes/${node}/bridges/${bridgeID}/start`)
-}
-
-export async function resetBridgeMetrics(bridgeId: string): Promise<void> {
-  return http.put(`/bridges/${bridgeId}/metrics/reset`)
-}
-
-export function testConnect(body: BridgeItem): Promise<void> {
-  return http.post(`/bridges_probe`, body)
-}
-
-export function queryBridgeMetrics(id: string): Promise<BridgeMetricsData> {
-  return http(`/bridges/${id}/metrics`)
-}
 
 //Rules
 export function getRules(
