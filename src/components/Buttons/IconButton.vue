6ee96e119e4f2277508efc69bcba3165bce4549d
Kinplemelon <wujiu529@gmail.com>
2025-09-23 16:56:17 +0800

    refactor(buttons): buttons for search

---
+++ src/components/Buttons/IconButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-button class="icon-button" plain><slot></slot></el-button>
+</template>
+
+<style lang="scss">
+.icon-button {
+  padding-left: 8px;
+  padding-right: 8px;
+  font-size: 14px;
+  .el-icon {
+    margin-right: 0;
+  }
+  [class*='el-icon'] + span {
+    margin-left: 0;
+  }
+}
+</style>
--- src/components/Buttons/SearchButton.vue
+++ src/components/Buttons/SearchButton.vue
@@ -1,7 +1,11 @@
 <template>
-  <el-button plain :icon="Search"></el-button>
+  <el-tooltip :content="tl('search')" placement="top" :hide-after="0">
+    <IconButton :icon="Search" />
+  </el-tooltip>
 </template>
 
 <script setup lang="ts">
 import { Search } from '@element-plus/icons-vue'
+
+const { tl } = useI18nTl('Base')
 </script>
