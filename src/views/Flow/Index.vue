dab9619d055885d0f75c2d3c4ba2b4999d815437
Kinplemelon <wujiu529@gmail.com>
2025-12-11 11:02:56 +0800

    fix(flow): enhance flow view logic and improve conditional rendering

--- src/views/Flow/Index.vue
+++ src/views/Flow/Index.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="flow" v-loading="isLoading">
-    <template v-if="showData">
-      <div class="flow-view-hd">
-        <el-row :gutter="20" class="flex-1" justify="space-between">
-          <el-col v-bind="colProps">
-            <NamespaceSelect
-              v-if="!isNamespaceUser"
-              v-model="selectedNamespace"
-              :clearable="false"
-              :global="{ enable: true, value: GLOBAL_NAMESPACE }"
-            />
-          </el-col>
-          <el-col v-bind="colProps">
-            <div class="flex justify-end">
-              <CreateButton @click="goCreate">
-                {{ tl('flow-create') }}
-              </CreateButton>
-            </div>
-          </el-col>
-        </el-row>
-      </div>
-      <FlowView
-        v-if="showBy === ShowByOpt.Flow"
-        :namespace="selectedNamespace"
-        @load="handleLoad"
-        @loaded="handleLoaded"
-      />
-    </template>
+    <div class="flow-view-hd" v-if="showHeader">
+      <el-row :gutter="20" class="flex-1" justify="space-between">
+        <el-col v-bind="colProps">
+          <NamespaceSelect
+            v-if="!isNamespaceUser"
+            v-model="selectedNamespace"
+            :clearable="false"
+            :global="{ enable: true, value: GLOBAL_NAMESPACE }"
+          />
+        </el-col>
+        <el-col v-bind="colProps">
+          <div class="flex justify-end">
+            <CreateButton v-if="hasFlowData" @click="goCreate">
+              {{ tl('flow-create') }}
+            </CreateButton>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <FlowView
+      v-if="hasFlowData && showBy === ShowByOpt.Flow"
+      :namespace="selectedNamespace"
+      @load="handleLoad"
+      @loaded="handleLoaded"
+    />
     <div v-else class="flow-placeholder-container">
-      <img class="img-placeholder" width="520" :src="getImgSrc()" alt="empty_placeholder" />
-      <CreateButton @click="goCreate">
-        {{ tl('flow-create') }}
-      </CreateButton>
+      <el-empty v-if="showEmpty" :description="t('Base.noData')" />
+      <template v-else>
+        <img class="img-placeholder" width="520" :src="getImgSrc()" alt="empty_placeholder" />
+        <CreateButton @click="goCreate">
+          {{ tl('flow-create') }}
+        </CreateButton>
+      </template>
     </div>
   </div>
 </template>
@@ -41,7 +42,7 @@ import { SEARCH_FORM_RES_PROPS as colProps } from '@/common/constants'
 import FlowView from './components/FlowView.vue'
 
 const router = useRouter()
-const { tl } = useI18nTl('components')
+const { tl, t } = useI18nTl('components')
 const store = useStore()
 
 const theme = computed(() => {
@@ -63,10 +64,22 @@ const enum ShowByOpt {
 const showBy = ref(ShowByOpt.Flow)
 
 const isLoading = ref(true)
-const showData = ref(true)
+const hasFlowData = ref(true)
 
 const selectedNamespace = ref<string>(GLOBAL_NAMESPACE)
 const isNamespaceUser = computed(() => store.getters.isNamespaceUser)
+
+const showHeader = computed(() => {
+  return !isNamespaceUser.value || (isNamespaceUser.value && hasFlowData.value)
+})
+const showEmpty = computed(() => {
+  return (
+    !isNamespaceUser.value &&
+    selectedNamespace.value &&
+    selectedNamespace.value !== GLOBAL_NAMESPACE &&
+    !hasFlowData.value
+  )
+})
 
 const handleLoad = () => {
   isLoading.value = true
@@ -74,7 +87,7 @@ const handleLoad = () => {
 
 const handleLoaded = (dataLength: number) => {
   isLoading.value = false
-  showData.value = dataLength > 0
+  hasFlowData.value = dataLength > 0
 }
 
 const goCreate = () => router.push({ name: 'flow-create' })
