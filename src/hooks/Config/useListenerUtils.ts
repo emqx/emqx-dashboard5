68dd1701753f5957bc25aa381b9a22f96e0396ad
Kinplemelon <wujiu529@gmail.com>
2026-01-15 17:11:21 +0800

    fix(listener): default value for `reuse_sessions`

--- src/hooks/Config/useListenerUtils.ts
+++ src/hooks/Config/useListenerUtils.ts
@@ -150,6 +150,7 @@ export default (gatewayName?: string | undefined): ListenerUtils => {
     cacertfile: '',
     keyfile: '',
     session_tickets: SSLSessionTickets.disabled,
+    reuse_sessions: true,
     verify: SSL_VERIFY_VALUE_MAP.get(true),
     fail_if_no_peer_cert: true,
     depth: 10,
