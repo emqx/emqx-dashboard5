9b6c5e97c0158d0e4226c00412d6dcdd33525230
Kinplemelon <wujiu529@gmail.com>
2026-05-27 18:14:08 +0800

    fix(audit): add cache size schema text

--- src/schemaText/schema-text-zh.ts
+++ src/schemaText/schema-text-zh.ts
@@ -395,6 +395,10 @@ export default {
     desc: '在数据库中存储最新的 N 条日志条目，以允许 `/audit` HTTP API 进行日志数据的过滤和检索。',
     label: '最大 Dashboard 记录数',
   },
+  cache_size: {
+    desc: '在数据库中存储最新的 N 条日志条目，以允许 `/audit` HTTP API 进行日志数据的过滤和检索。',
+    label: '缓存大小',
+  },
   ignore_high_frequency_request: {
     desc: '忽略高频请求以避免淹没审计日志，例如被忽略的发布/订阅踢出 http api 请求。',
     label: '忽略高频请求',
