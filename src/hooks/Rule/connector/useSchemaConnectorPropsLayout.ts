a6f3afa914789249f3cf15da472f9d04a6b191ad
Kinplemelon <wujiu529@gmail.com>
2026-05-27 18:25:21 +0800

    fix(emqx-tables): expose ttl and ts_column fields

--- src/hooks/Rule/connector/useSchemaConnectorPropsLayout.ts
+++ src/hooks/Rule/connector/useSchemaConnectorPropsLayout.ts
@@ -447,7 +447,6 @@ export default (
     [BridgeType.Datalayers]: getDatalayersColClass,
     [BridgeType.KafkaProducer]: getKafkaProducerColClass,
     [BridgeType.Confluent]: getKafkaProducerColClass,
-    [BridgeType.EMQXTables]: { ttl: 'col-hidden', ts_column: 'col-hidden' },
   }
 
   const pgSqlAdvancedFields = ['disable_prepared_statements']
