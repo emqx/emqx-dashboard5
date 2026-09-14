16aa3791343c1b763e5a295fbca4dd38d66f450a
Kinplemelon <wujiu529@gmail.com>
2026-09-14 11:20:14 +0800

    fix(connector): add 'socket_opts.ip_family' to advanced properties and include Confluent in the order map

--- src/hooks/Rule/connector/useSchemaConnectorPropsLayout.ts
+++ src/hooks/Rule/connector/useSchemaConnectorPropsLayout.ts
@@ -69,6 +69,7 @@ export default (
     'socket_opts.recbuf',
     'socket_opts.tcp_keepalive',
     'socket_opts.nodelay',
+    'socket_opts.ip_family',
   ]
 
   const IoTDBAdvancedProps = ['recv_timeout', 'enable_pipelining']
@@ -169,6 +170,7 @@ export default (
     [BridgeType.AzureEventHubs]: azureOrderMap,
     [BridgeType.KafkaProducer]: azureOrderMap,
     [BridgeType.KafkaConsumer]: azureOrderMap,
+    [BridgeType.Confluent]: azureOrderMap,
     [BridgeType.PgSQL]: pgSqlOrderMap,
     [BridgeType.TimescaleDB]: pgSqlOrderMap,
     [BridgeType.MatrixDB]: pgSqlOrderMap,
